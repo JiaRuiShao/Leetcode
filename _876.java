5092d52aca5eee525bbaad4f162e06f8c6d1c2cb
Jessie Shao <jiaruis@andrew.cmu.edu>
2022-02-15 15:24:03 -0500

    876. Middle of the Linked List

---
+++ _876.java
@@ -0,0 +1,14 @@
+import helper.ListNode;
+
+public class _876 {
+    class Solution {
+        public ListNode middleNode(ListNode head) {
+            ListNode slow = head, fast = head;
+            while (fast != null && fast.next != null) {
+                fast = fast.next.next;
+                slow = slow.next;
+            }
+            return slow;
+        }
+    }
+}
