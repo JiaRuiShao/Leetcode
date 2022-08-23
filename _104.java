002ff2b4d3444b74b4f8f5f5e928f63203429da3
Jessie Shao <jiaruis@andrew.cmu.edu>
2022-08-22 19:38:32 -0500

    104. Maximum Depth of Binary Tree

--- _104.java
+++ _104.java
@@ -1,6 +1,4 @@
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
 
 /**
  * 104. Maximum Depth of Binary Tree
@@ -56,7 +54,7 @@ public class _104 {
         private void traverse(TreeNode curr) {
             if (curr == null) return;
             height++;
-            if (height > maxHeight) maxHeight = height;
+            if (height > maxHeight) maxHeight = height; // pre-order traverse
             traverse(curr.left);
             traverse(curr.right);
             height--;
@@ -65,7 +63,7 @@ public class _104 {
 
     class Solution3_BFS {
         /**
-         * Level-order traverse.
+         * Level-order traverse using null as an indicator of level end.
          * Time: O(n)
          * Space: O(n) when the tree is balanced
          * @param root
@@ -85,6 +83,63 @@ public class _104 {
                 if (q.peek() == null) { // move to the next level/depth
                     q.offer(q.poll()); // add the null back to the queue
                     maxDepth++;
+                }
+            }
+            return maxDepth;
+        }
+
+        /**
+         * Normal BFS.
+         * @param root
+         * @return
+         */
+        public int maxDepth2(TreeNode root) {
+            int maxDepth = 0;
+            if (root == null) return maxDepth;
+            Queue<TreeNode> q = new LinkedList<>();
+            TreeNode curr;
+            q.offer(root);
+            while (!q.isEmpty()) {
+                maxDepth++;
+                int sz = q.size();
+                for (int i = 0; i < sz; i++) {
+                    curr = q.poll();
+                    if (curr.left != null) q.offer(curr.left);
+                    if (curr.right != null) q.offer(curr.right);
+                }
+            }
+            return maxDepth;
+        }
+    }
+
+    class Solution4_DFS_Iterative {
+        /**
+         * Time: O(n)
+         * Space: O(2n) = O(n) if skewed
+         * @param root
+         * @return
+         */
+        public int maxDepth(TreeNode root) {
+            int maxDepth = 0;
+            if (root == null) return maxDepth;
+            Deque<TreeNode> stack = new LinkedList<>();
+            Deque<Integer> depth = new LinkedList<>();
+            stack.push(root);
+            depth.push(1);
+
+            TreeNode curr = root;
+            int currDepth = 0;
+            while (!stack.isEmpty()) {
+                curr = stack.pop();
+                currDepth = depth.pop();
+                if (currDepth > maxDepth) maxDepth = currDepth;
+                if (curr.right != null) {
+                    stack.push(curr.right);
+                    depth.push(currDepth + 1);
+                }
+                if (curr.left != null) {
+                    stack.push(curr.left);
+                    depth.push(currDepth + 1);
                 }
             }
             return maxDepth;
