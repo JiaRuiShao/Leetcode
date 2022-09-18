fba3f3deae7de99fef769020d6057943a43ea64d
Jessie Shao <jiaruis@andrew.cmu.edu>
2022-09-18 18:58:22 -0500

    230. Kth Smallest Element in a BST

--- _230.java
+++ _230.java
@@ -1,14 +1,25 @@
 import helper.TreeNode;
 
+/**
+ * 230. Kth Smallest Element in a BST.
+ */
 public class _230 {
-    class Solution {
-        int n = 0;
+    static class Solution_BFS_Traversal {
+        int rank = 0;
         int val = 0;
 
+        /**
+         * In order Traversal.
+         * Time: O(n)
+         * Space: O(h)
+         *  
+         * @param root root
+         * @param k kth smallest element
+         */
         private void traverse(TreeNode root, int k) {
-            if (root == null || n >= k) return;
+            if (root == null || rank >= k) return;
             traverse(root.left, k);
-            if (++n == k) val = root.val;
+            if (++rank == k) val = root.val;
             traverse(root.right, k);
         }
 
