a29469e1805ee6121d6fc266bb853012c4901de1
Jessie Shao <jiaruis@andrew.cmu.edu>
2022-09-18 19:15:08 -0500

    538. Convert BST to Greater Tree

--- _538.java
+++ _538.java
@@ -1,9 +1,12 @@
 import helper.TreeNode;
 
+/**
+ * 538. Convert BST to Greater Tree.
+ */
 public class _538 {
-    class Solution {
+    class Solution_BFS_Traversal {
 
-        private int sum = 0;
+        private int sum = 0; // record the sum that larger then current node
 
         private void traverse(TreeNode root) {
             // base case
