09f615d1efcc6169b2fe78918bc5f475c1ed112a
Jessie Shao <jiaruis@andrew.cmu.edu>
2022-10-01 16:45:23 -0500

    98. Validate Binary Search Tree.

--- _98.java
+++ _98.java
@@ -1,7 +1,18 @@
 import helper.TreeNode;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * 98. Validate Binary Search Tree.
+ * Given the root of a binary tree, determine if it is a valid binary search tree (BST).
+ * A valid BST is defined as follows:
+ *  - The left subtree of a node contains only nodes with keys less than the node's key.
+ * - The right subtree of a node contains only nodes with keys greater than the node's key.
+ * - Both the left and right subtrees must also be binary search trees.
+ */
 public class _98 {
-    class Solution {
+    class Solution1_Recursive_DP {
         /**
          * Compare the largest val in the left subtree & the smallest val
          * in the right subtree with the root.val.
@@ -35,5 +46,59 @@ public class _98 {
             // pass the root val in the left subtree as the max value, in the right subtree as the min value
             return valid(root.left, min, root.val) && valid(root.right, root.val, max);
         }
+    }
+
+    class Solution2_Recursive_Traverse {  // valid bst in-order traverse should be in ascending order
+        Integer prev = null;
+        boolean isBST = true;
+
+        public boolean isValidBST(TreeNode root) {
+            traverse(root);
+            return isBST;
+        }
+
+        private void traverse(TreeNode curr) {
+            if (curr == null || !isBST) return;
+            traverse(curr.left);
+            if (prev != null && prev >= curr.val) isBST = false;
+            prev = curr.val;
+            traverse(curr.right);
+        }
+    }
+
+    class Solution3_Iterative_Traverse {
+        public boolean isValidBST(TreeNode root) {
+            Deque<TreeNode> stack = new ArrayDeque<>();
+            TreeNode visited = new TreeNode(-1);
+            Integer prev = null;
+
+            pushLeftBranch(root, stack);
+            while (!stack.isEmpty()) {
+                TreeNode curr = stack.peek();
+                if ((curr.left == null || curr.left == visited) && curr.right != visited) {
+                    // In-Order Position
+                    if (prev != null && prev >= curr.val) return false;
+                    prev = curr.val;
+                    pushLeftBranch(curr.right, stack);
+                }
+
+                if (curr.right == null || curr.right == visited) {
+                    visited = stack.pop();
+                }
+            }
+            return true;
+        }
+
+        private void pushLeftBranch(TreeNode root, Deque<TreeNode> stack) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+        }
+    }
+
+    class Solution4_Iterative_DP {
+
+
     }
 }
