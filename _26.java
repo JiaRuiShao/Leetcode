1a5997b4add4184d53186b441de553e98765b93f
Jessie Shao <jiaruis@andrew.cmu.edu>
2022-10-08 01:13:31 -0500

    26. Remove Duplicates from Sorted Array

--- _26.java
+++ _26.java
@@ -1,15 +1,30 @@
+/**
+ * 26. Remove Duplicates from Sorted Array.
+ */
 public class _26 {
-    class Solution {
+    class Solution_Fast_Slow_Pointers {
         public int removeDuplicates(int[] nums) {
+            // slow points to the end of the non-dup numbers, fast points to the next diff number
+            int slow = 0, fast = 0, n = nums.length;
+            while (fast < n) {
+                if (nums[fast] != nums[slow]) {
+                    nums[++slow] = nums[fast];
+                }
+                fast++;
+            }
+            return slow + 1;
+        }
+    
+        public int removeDuplicates2(int[] nums) {
             if (nums == null || nums.length == 0) return 0;
-            int left = 0, right = 0; // unique nums are records at the left (& equal to) of the left right
-            while (++right < nums.length) {
-                if (nums[right] != nums[left]) {
-                    left++;
-                    if (left != right) nums[left] = nums[right];
+            int slow = 0, fast = 0; // unique nums are records at the slow (& equal to) of the slow fast
+            while (++fast < nums.length) {
+                if (nums[fast] != nums[slow]) {
+                    slow++;
+                    if (slow != fast) nums[slow] = nums[fast];
                 }
             }
-            return left + 1;
+            return slow + 1;
         }
     }
 }
