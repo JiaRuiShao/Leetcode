bf10c341b96c5352cc4af08e7ca3aeb554b02d7a
Jessie Shao <jiaruis@andrew.cmu.edu>
2022-10-08 17:47:06 -0500

    27. Remove Element

--- _27.java
+++ _27.java
@@ -1,5 +1,30 @@
+/**
+ * 27. Remove Element.
+ *
+ * Given an integer array nums and an integer val, remove all occurrences of val
+ * in nums in-place. The relative order of the elements may be changed.
+ * More formally, if there are k elements after removing the duplicates, then the first k elements of nums should hold the final result.
+ * It does not matter what you leave beyond the first k elements.
+ * Return k after placing the final result in the first k slots of nums.
+ *
+ * Do not allocate extra space for another array.
+ * You must do this by modifying the input array in-place with O(1) extra memory.
+ */
 public class _27 {
-    class Solution {
+    class Solution1_Fast_Slow_Pointer {
+        public int removeElement(int[] nums, int val) {
+            int slow = 0, fast = 0;
+            while (fast < nums.length) {
+                if (nums[fast] != val) {
+                    nums[slow++] = nums[fast];
+                }
+                fast++;
+            }
+            return slow;
+        }
+    }
+    
+    class Solution2_Left_Right_Pointer {
         public int removeElement(int[] nums, int val) {
             if (nums == null || nums.length == 0) return 0;
             int i = 0, j = nums.length - 1;
