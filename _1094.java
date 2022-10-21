f25cd6b445e545a549b59dc63b54490c23fd68f4
Jessie Shao <jiaruis@andrew.cmu.edu>
2022-10-20 19:32:15 -0500

    1094. Car Pooling

--- _1094.java
+++ _1094.java
@@ -1,5 +1,11 @@
+import java.util.Arrays;
+import java.util.OptionalInt;
+
+/**
+ * 1094. Car Pooling.
+ */
 public class _1094 {
-    static class Solution {
+    class Solution1 {
         /**
          * Car Pooling.
          *
@@ -25,9 +31,42 @@ public class _1094 {
             return true;
         }
     }
+    
+    class Solution2 {
+        public boolean carPooling(int[][] trips, int capacity) {
+            int tripLen = findTripLen(trips);
+            int[] diff = new int[tripLen];
+            for (int[] trip : trips) {
+                update(diff, trip[1], trip[2] - 1, trip[0]);
+            }
+            
+            int[] passengers = new int[tripLen];
+            for (int i = 0; i < tripLen; i++) {
+                if (i == 0) {
+                    passengers[i] = diff[i];
+                } else {
+                    passengers[i] = diff[i] + passengers[i - 1];
+                }
+                if (passengers[i] > capacity) return false;
+            }
+            return true;
+        }
+        
+        private int findTripLen(int[][] trips) {
+            OptionalInt maxTrip = Arrays.stream(trips).mapToInt(trip -> trip[2]).max();
+            return maxTrip.isPresent() ? maxTrip.getAsInt() : -1;
+        }
+        
+        private void update(int[] diff, int from, int to, int numPass) {
+            diff[from] += numPass;
+            if (to + 1 < diff.length) {
+                diff[to + 1] -= numPass;
+            }
+        }
+    }
 
     public static void main(String[] args) {
         int[][] trips = new int[][]{{9,0,1},{3,3,7}};
-        new Solution().carPooling(trips, 4);
+        new _1094().new Solution1().carPooling(trips, 4);
     }
 }
