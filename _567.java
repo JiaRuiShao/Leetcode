3f7d08a93ea98934adfb9a5953e17b72b0b4092e
Jessie Shao <jiaruis@andrew.cmu.edu>
2022-10-24 10:00:40 -0500

    567. Permutation in String

--- _567.java
+++ _567.java
@@ -1,5 +1,61 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 567. Permutation in String.
+ */
 public class _567 {
-    static class Solution {
+    class Solution1 {
+        public boolean checkInclusion(String s1, String s2) {
+            // build a freq map for target s1
+            int size = s1.length();
+            Map<Character, Integer> freq = new HashMap<>();
+            for (int i = 0; i < size; i++) {
+                freq.put(s1.charAt(i), freq.getOrDefault(s1.charAt(i), 0) + 1);
+            }
+            
+            // left & right pointers to construct a window that satisfy the requirement
+            int left = 0, right = 0, valid = 0;
+            while (right < s2.length()) {
+                // char adding into the window
+                char c = s2.charAt(right);
+                right++; // update the upper bound
+                
+                // update the freq map and the valid count
+                if (freq.containsKey(c)) {
+                    freq.put(c, freq.get(c) - 1);
+                    if (freq.get(c) >= 0) {
+                        valid++;
+                    }
+                }
+                
+                // update the ans & freq map when window.size is target size
+                // notice here we didn't use while loop while (valid == targetK)
+                // cuz the requirement here is no other chars in the middle of the substring
+                if (right - left == size) {
+                    // requirement met
+                    if (valid == size) {
+                        return true;
+                    }
+                    
+                    // char popping out of the window
+                    c = s2.charAt(left);
+                    left++; // update the lower bound
+                    
+                    // update the freq map and the valid count
+                    if (freq.containsKey(c)) {
+                        freq.put(c, freq.get(c) + 1);
+                        if (freq.get(c) > 0) {
+                            valid--;
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+    }
+    
+    class Solution2 {
         static final int MAX_CHAR = 256;
         public boolean checkInclusion(String s1, String s2) {
             int[] count = new int[MAX_CHAR]; // ASCII size counter array
