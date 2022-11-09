96b526364de4223ba03f9a8615bda294e3f8a24c
Jessie Shao <jiaruis@andrew.cmu.edu>
2022-11-09 10:28:20 -0600

    264. Ugly Number II

--- _264.java
+++ _264.java
@@ -6,25 +6,42 @@ public class _264 {
 		new Wrong_Solution().nthUglyNumber(11); // returns 14, which is not an ugly number
 	}
 	
-	static class Wrong_Solution {
+	static class Solution_Time_Limit_Exceeded {
+		/**
+		 * Time: O(nlogn)
+		 * Space: O(1)
+		 *
+		 * @param n nth ugly number
+		 * @return the nth ugly number whose prime factor are: 2, 3, and 5
+		 */
 		public int nthUglyNumber(int n) {
 			int num = 0;
 			while (n > 0) {
-				num++;
-				System.out.printf("n is %d, num is %d%n", n, num);
-				if (isUglyNumber(num)) {
+				if (isUgly(++num)) {
 					n--;
 				}
 			}
 			return num;
 		}
 		
-		private boolean isUglyNumber(int num) {
-			return num == 1 || num % 2 == 0 || num % 3 == 0 || num % 5 == 0;
+		/**
+		 * Time: O(log_2(n) + log_3(n) + log_5(n))
+		 * Space: O(1)
+		 *
+		 * @param n given num
+		 * @return true if the given num is an ugly num; false if not
+		 */
+		private boolean isUgly(int n) {
+			if (n <= 0) return false;
+			while (n % 2 == 0) n /= 2;
+			while (n % 3 == 0) n /= 3;
+			while (n % 5 == 0) n /= 5;
+			return n == 1;
 		}
 	}
 	
 	static class Solution_MergeLinkedList {
+		
 		/**
 		 * Think this question of merging three linked list, compare the product for each list and update the ugly array with the smallest one.
 		 * Time: O(n)
@@ -55,6 +72,26 @@ public class _264 {
 		
 		private int findSmallest(int p1, int p2, int p3) {
 			return Math.min(Math.min(p1, p2), p3);
+		}
+		
+	}
+	
+	static class Wrong_Solution {
+		public int nthUglyNumber(int n) {
+			int num = 0;
+			while (n > 0) {
+				num++;
+				System.out.printf("n is %d, num is %d%n", n, num);
+				if (isUglyNumber(num)) {
+					n--;
+				}
+			}
+			return num;
+		}
+		
+		// wrong: 14 shouldn't be returned as true
+		private boolean isUglyNumber(int num) {
+			return num == 1 || num % 2 == 0 || num % 3 == 0 || num % 5 == 0;
 		}
 	}
 }
