6f0c0a026a5936d26dfa3c5f90dbc34a5a28dce8
JiaRuiShao <jiaruishaojessie@gmail.com>
2024-03-03 13:17:41 -0600

    74. Search a 2D Matrix

--- _240.java
+++ _240.java
@@ -30,4 +30,26 @@ public class _240 {
 			return false;
 		}
 	}
+
+	class Solution2 {
+		public boolean searchMatrix(int[][] matrix, int target) {
+			return findTarget(matrix, target);
+		}
+	
+		private boolean findTarget(int[][] matrix, int target) {
+			int m = matrix.length, n = matrix[0].length;
+			int row = 0, col = n - 1;
+			while (row < m && col >= 0) {
+				int val = matrix[row][col];
+				if (val == target) {
+					return true;
+				} else if (val < target) {
+					row++;
+				} else {
+					col--;
+				}
+			}
+			return false;
+		}
+	}
 }
--- _74.java
+++ _74.java
@@ -1,5 +1,6 @@
 /**
  * 74. Search a 2D Matrix.
+ * When unflatten the 1D matrix to 2D, we only need its col size
  */
 public class _74 {
 	/**
@@ -26,5 +27,32 @@ public class _74 {
 			}
 		}
 		return false;
+	}
+
+	class Solution2 {
+		public boolean searchMatrix(int[][] matrix, int target) {
+			return findTarget(matrix, target);
+		}
+	
+		private boolean findTarget(int[][] matrix, int target) {
+			int row = matrix.length, col = matrix[0].length;
+			int lo = 0, hi = row * col - 1;
+			while (lo <= hi) {
+				int mid = lo + (hi - lo) / 2;
+				int val = findMatrixVal(matrix, col, mid);
+				if (val == target) {
+					return true;
+				} else if (val < target) {
+					lo = mid + 1;
+				} else {
+					hi = mid - 1;
+				}
+			}
+			return false;
+		}
+	
+		private int findMatrixVal(int[][] matrix, int c, int idx) {
+			return matrix[idx / c][idx % c];
+		}
 	}
 }
