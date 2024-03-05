1764c9bd1ba080f3e8d13312ec8d2232780216ae
JiaRuiShao <jiaruishaojessie@gmail.com>
2024-03-04 19:47:23 -0600

    567. Permutation in String

--- _567.java
+++ _567.java
@@ -3,6 +3,7 @@ import java.util.Map;
 
 /**
  * 567. Permutation in String.
+ * Count each needed character not just the distinct character -- this will effect how we update the needed chars
  */
 public class _567 {
     class Solution1 {
@@ -73,6 +74,134 @@ public class _567 {
                     if (counter == 0) return true;
                     // move left pointer
                     if (++count[s2.charAt(left++)] > 0) counter++;
+                }
+            }
+            return false;
+        }
+    }
+
+    class Solution3 {
+
+        private int MAX_CHAR = 256;
+    
+        /**
+         * We can use char array here because the constraint mentioned 1 <= s1.length, s2.length <= 10^4, and each char has 2 bytes, which is 16 bits to store the data. 2^16 - 1 - 10^4 > 0, it means we can store the data as char without overflow problem.
+         * Notice that char is stored as ASCII code in java, which is unsigned, so it can store up to 16 bits numbers
+         * Improvements: we can store the lowercase english letters in a 26-size int arr
+         * @param s1 s1
+         * @param s2 s2
+         * @return true if s1's permutation found in s2; else false
+         */
+        public boolean checkInclusion(String s1, String s2) {
+            char[] freq = new char[MAX_CHAR];
+            for (int i = 0; i < s1.length(); i++) {
+                freq[s1.charAt(i)]++;
+            }
+    
+            int l = 0, r = 0, maxWinLen = s1.length(), need = s1.length();
+            while(r < s2.length()) {
+                char cAdd = s2.charAt(r++);
+                if (freq[cAdd] > 0) {
+                    need--;
+                }
+                freq[cAdd]--;
+    
+                if (r - l == maxWinLen) {
+                    if (need == 0) {
+                        return true;
+                    }
+                    char cRem = s2.charAt(l++);
+                    freq[cRem]++;
+                    if (freq[cRem] > 0) {
+                        need++;
+                    }
+                }
+            }
+    
+            return false;
+        }
+    }
+
+    class Solution4 {
+
+        private char a = 'a';
+    
+        private short convertCharToShort(char c) {
+            return (short) (c - a);
+        }
+    
+        /**
+         * Uses short array to store the char freq. Compared to char array and int array, it saves some more memory space. But is it really worth it?
+         * @param s1 s1
+         * @param s2 s2
+         * @return true if s1's permutation found in s2; else false
+         */
+        public boolean checkInclusion(String s1, String s2) {
+            short[] freq = new short[26];
+            int maxWinLen = s1.length();
+            for (int i = 0; i < maxWinLen; i++) {
+                freq[convertCharToShort(s1.charAt(i))]++;
+            }
+    
+            int l = 0, r = 0, need = maxWinLen;
+            short cAdd = 0, cRem = 0;
+            while(r < s2.length()) {
+                cAdd = convertCharToShort(s2.charAt(r++));
+                if (freq[cAdd] > 0) {
+                    need--;
+                }
+                freq[cAdd]--;
+    
+                if (r - l == maxWinLen) {
+                    if (need == 0) {
+                        return true;
+                    }
+                    cRem = convertCharToShort(s2.charAt(l++));
+                    freq[cRem]++;
+                    if (freq[cRem] > 0) {
+                        need++;
+                    }
+                }
+            }
+    
+            return false;
+        }
+    }
+
+    /**
+     * NO-BRAINER SOLUTION! Uses two freq map for both strings.
+     * Ref: https://labuladong.online/algo/ds-class/shu-zu-lia-39fd9/hua-dong-c-c0f54/#%E4%BA%8C%E3%80%81%E5%AD%97%E7%AC%A6%E4%B8%B2%E6%8E%92%E5%88%97
+     */
+    class Solution5 {
+        public boolean checkInclusion(String t, String s) {
+            HashMap<Character, Integer> need = new HashMap<>();
+            HashMap<Character, Integer> window = new HashMap<>();
+            for (int i = 0; i < t.length(); i++) {
+                char c = t.charAt(i);
+                need.put(c, need.getOrDefault(c, 0) + 1);
+            }
+    
+            int left = 0, right = 0;
+            int valid = 0;
+            while (right < s.length()) {
+                char c = s.charAt(right);
+                right++;
+                if (need.containsKey(c)) {
+                    window.put(c, window.getOrDefault(c, 0) + 1);
+                    if (window.get(c).equals(need.get(c)))
+                        valid++;
+                }
+    
+                while (right - left >= t.length()) {
+                    if (valid == need.size())
+                        return true;
+                    char d = s.charAt(left);
+                    left++;
+                    if (need.containsKey(d)) {
+                        if (window.get(d).equals(need.get(d)))
+                            valid--;
+                        window.put(d, window.getOrDefault(d, 0) - 1);
+                    }
                 }
             }
             return false;
