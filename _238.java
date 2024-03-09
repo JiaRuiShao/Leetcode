2799344c4ed914560659989bfbe6473773b94567
JiaRuiShao <jiaruishaojessie@gmail.com>
2024-03-09 13:00:19 -0600

    238. Product of Array Except Self

--- _238.java
+++ _238.java
@@ -46,4 +46,44 @@ public class _238 {
             return ans;
         }
     }
+
+    class Solution2_Prefix_Suffix {
+        public int[] productExceptSelf(int[] nums) {
+            int n = nums.length;
+            int[] pre = new int[n + 1]; // pre[i] stores the product of elements [0, i-1]
+            int[] suf = new int[n + 1]; // suf[i] stores the product of elements [i+1, n-1]
+            pre[0] = 1;
+            suf[n] = 1;
+            for (int i = 0; i < n; i++) {
+                pre[i + 1] = pre[i] * nums[i];
+                int j = n - 1 - i;
+                suf[j] = suf[j + 1] * nums[j];
+            }
+            
+            int[] prod = new int[n];
+            for (int i = 0; i < n; i++) {
+                // left: [0...i-1]
+                // right: [i+1, n-1]
+                prod[i] = pre[i] * suf[i + 1];
+            }
+            return prod;
+        }
+    }
+
+    class Solution3_Prefix_Suffix_Improved {
+        public int[] productExceptSelf(int[] nums) {
+            int n = nums.length;
+            int[] result = new int[n];
+            result[0] = 1;
+            for (int i = 1; i < n; i++) {
+                result[i] = result[i - 1] * nums[i - 1];
+            }
+            int right = 1;
+            for (int i = n - 1; i >= 0; i--) {
+                result[i] *= right;
+                right *= nums[i];
+            }
+            return result;
+        }
+    }
 }
