8ee91a78e3859373e9f51fa9566515b0a7c1f544
JiaRuiShao <jiaruishaojessie@gmail.com>
2024-03-09 13:00:53 -0600

    724. Find Pivot Index

--- _724.java
+++ _724.java
@@ -56,4 +56,20 @@ public class _724 {
 			return leftSum == rightSum;
 		}
 	}
+
+	class Solution3 {
+		public int pivotIndex(int[] nums) {
+            int total = 0;
+            for (int num : nums) {
+                total += num;
+            }
+            int sum = 0;
+            for (int i = 0; i < nums.length; sum += nums[i++]) {
+                if (sum * 2 == total - nums[i]) {
+                    return i;
+                }
+            }
+            return -1;
+        }
+	}
 }
