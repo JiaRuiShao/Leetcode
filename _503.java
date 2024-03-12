9cbf6f0ba0e422d7603cba902893e083e11a01dd
JiaRuiShao <jiaruishaojessie@gmail.com>
2024-03-11 22:14:03 -0500

    503. Next Greater Element II

--- _503.java
+++ _503.java
@@ -1,18 +1,32 @@
 import java.util.ArrayDeque;
 import java.util.Deque;
 
+/**
+ * 503. Next Greater Element II.
+ * Traverse two times to make sure we get all NGE for all elements.
+ * Use modulo to get the actual index.
+ */
 public class _503 {
     class Solution {
+        /**
+         * Monotonic Increasing Stack.
+         * Time: O(n)
+         * Space: O(n)
+         * 
+         * @param nums circular array
+         * @return next greater elements
+         */
         public int[] nextGreaterElements(int[] nums) {
             int n = nums.length;
             int[] res = new int[n];
             Deque<Integer> stack = new ArrayDeque<Integer>(n);
-            for (int i = 2*n - 1; i >= 0; i--) {
-                while (!stack.isEmpty() && stack.peekLast() <= nums[i % n]) {
-                    stack.removeLast();
+            for (int i = 2 * n - 1; i >= 0; i--) {
+                int idx = i % n;
+                while (!stack.isEmpty() && stack.peek() <= nums[idx]) {
+                    stack.pop();
                 }
-                res[i % n] = stack.isEmpty() ? -1 : stack.peekLast();
-                stack.addLast(nums[i % n]);
+                res[idx] = stack.isEmpty() ? -1 : stack.peek();
+                stack.push(nums[idx]);
             }
             return res;
         }
