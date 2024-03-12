0256f65ade8eab8ce3e30149ccb9b005d689a4e2
JiaRuiShao <jiaruishaojessie@gmail.com>
2024-03-11 21:35:24 -0500

    496. Next Greater Element I

--- _496.java
+++ _496.java
@@ -2,9 +2,54 @@ import java.util.Deque;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Map;
+import java.util.Stack;
 
+/**
+ * 496. Next Greater Element I.
+ */
 public class _496 {
-    public static class Solution2 {
+    /**
+     * Monotonic Stack & HashMap.
+     * Time: O(n + m) = O(m) where n is the length of subset nums1 and m is the length of the array nums2
+     * Space: O(2m) = O(m) where m is length of nums2
+     */
+    class Solution1 {
+        private int[] findNGLMappings(int[] nums, Map<Integer, Integer> map, int[] ngl) {
+            int[] res = new int[nums.length];
+            for (int i = 0; i < nums.length; i++) {
+                int idxInNums2 = map.get(nums[i]);
+                res[i] = ngl[idxInNums2];
+            }
+            return res;
+        }
+
+        private int[] findNextGreaterElem(int[] nums, Map<Integer, Integer> map) {
+            int n = nums.length;
+            int[] ngl = new int[n];
+            Stack<Integer> stack = new Stack<>();
+            for (int i = n - 1; i >= 0; i--) {
+                int num = nums[i];
+                map.put(num, i);
+
+                while (!stack.isEmpty() && stack.peek() <= num) {
+                    stack.pop();
+                }
+
+                ngl[i] = stack.isEmpty() ? -1 : stack.peek();
+                stack.push(num);
+            }
+
+            return ngl;
+        }
+
+        public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+            Map<Integer, Integer> valueIdxMap = new HashMap<>();
+            int[] ngl = findNextGreaterElem(nums2, valueIdxMap);
+            return findNGLMappings(nums1, valueIdxMap, ngl);
+        }
+    }
+
+    class Solution2 {
         /**
          * Use monotonic stack to get the next greater elements for each number in O(n) time complexity.
          */
