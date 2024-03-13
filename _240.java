c9a98a70091a3042e69c93ddd0abe402d7662d27
JiaRuiShao <jiaruishaojessie@gmail.com>
2024-03-13 17:26:09 -0500

    240. Search a 2D Matrix II

--- _240.java
+++ _240.java
@@ -2,11 +2,14 @@
  * 240. Search a 2D Matrix II.
  */
 public class _240 {
-	class Solution_Binary_Search {
+	/**
+	 * Notice that if we use backtracking on this problem, the time complexity will be 4^(mn).
+	 */
+	class Solution1_Binary_Search {
 		/**
-		 * Start from upper right corner (or lower left corner), go down or left until find the target or index out of bound.
-		 * Why start from upper right corner? If the elem is < target, go down, if it's > target go left.
-		 * If we start from the upper left corner, we don't know where to go (both go down & go right are larger numbers)
+		 * Start from top-right corner (or bottom-left corner), go down or left until find the target or index out of bound.
+		 * Why start from top-right corner? If the elem is < target, go down, if it's > target go left.
+		 * If we start from the top-left or bottom-right corner, we don't know where to go (both go down & go right are larger numbers)
 		 *
 		 * Time: O(m+n)
 		 * Space: O(1)
@@ -15,23 +18,6 @@ public class _240 {
 		 * @param target target number to find
 		 * @return true if target found; else false
 		 */
-		public boolean searchMatrix(int[][] matrix, int target) {
-			int m = matrix.length, n = matrix[0].length;
-			int i = 0, j = n - 1; // start from upper right
-			while (i < m && j >= 0) { // terminate when i == m || j == -1
-				if (matrix[i][j] == target) {
-					return true;
-				} else if (matrix[i][j] < target) {
-					i++;
-				} else if (matrix[i][j] > target) {
-					j--;
-				}
-			}
-			return false;
-		}
-	}
-
-	class Solution2 {
 		public boolean searchMatrix(int[][] matrix, int target) {
 			return findTarget(matrix, target);
 		}
@@ -47,6 +33,33 @@ public class _240 {
 					row++;
 				} else {
 					col--;
+				}
+			}
+			return false;
+		}
+	}
+
+	class Solution2_Binary_Search {
+		/**
+		 * Start from bottom-left corner.
+		 * Time: O(m+n)
+		 * Space: O(1)
+		 * 
+		 * @param matrix int matrix
+		 * @param target target number to find
+		 * @return true if target found; else false
+		 */
+		public boolean searchMatrix(int[][] matrix, int target) {
+			int m = matrix.length, n = matrix[0].length;
+			int row = m - 1, col = 0;
+			while (row >= 0 && col < n) {
+				int cmp = matrix[row][col] - target;
+				if (cmp == 0) {
+					return true;
+				} else if (cmp < 0) {
+					col++;
+				} else {
+					row--;
 				}
 			}
 			return false;
