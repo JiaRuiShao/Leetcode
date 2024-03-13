3c4b0b098ee3364f3f7a8b3b981ad520e4ba27b3
JiaRuiShao <jiaruishaojessie@gmail.com>
2024-03-13 11:56:12 -0500

    18. 4Sum

--- _18.java
+++ _18.java
@@ -4,20 +4,18 @@ import java.util.List;
 
 /**
  * 18. 4Sum.
+ * Integer Overflow as 3 * 10^9 > 2^31 - 1
  */
 public class _18 {
-	class Solution {
+	class Solution1 {
 		public List<List<Integer>> fourSum(int[] nums, int target) {
-			// n^2logn+nlogn
 			int n = nums.length;
 			Arrays.sort(nums);
-			// 0, 1, 2, 3
 			List<List<Integer>> quadruplets = new ArrayList<>();
 			for (int a = 0; a < n - 3; a++) {
 				if (a > 0 && nums[a] == nums[a - 1]) continue;
 				for (int b = a + 1; b < n - 2; b++) {
 					if (b > a + 1 && nums[b] == nums[b - 1]) continue;
-					// binary search
 					int c = b + 1, d = n - 1;
 					while (c < d) {
 						int left = nums[c], right = nums[d];
@@ -31,6 +29,58 @@ public class _18 {
 						} else {
 							d--;
 						}
+					}
+				}
+			}
+			return quadruplets;
+		}
+	}
+
+	class Solution2 {
+
+		private List<List<Integer>> twoSum(int[] nums, long target, int left, int right) {
+			List<List<Integer>> twins = new ArrayList<>();
+			while (left < right) {
+				int leftVal = nums[left];
+				int rightVal = nums[right];
+				long cpr = leftVal + rightVal - target;
+				if (cpr == 0) {
+					List<Integer> twin = new ArrayList<>();
+					twin.add(leftVal);
+					twin.add(rightVal);
+					twins.add(twin);
+					while (left < right && nums[left] == leftVal) left++;
+					while (left < right && nums[right] == rightVal) right--;
+				} else if (cpr > 0) {
+					while (left < right && nums[right] == rightVal) right--;
+				} else {
+					while (left < right && nums[left] == leftVal) left++;
+				}
+			}
+			
+			return twins;
+		}
+	
+		public List<List<Integer>> fourSum(int[] nums, int target) {
+			int n = nums.length;
+			List<List<Integer>> quadruplets = new ArrayList<>();
+			if (n < 4) {
+				return quadruplets;
+			}
+			Arrays.sort(nums);
+			for (int i = 0; i < n - 3; i++) {
+				if (i > 0 && nums[i] == nums[i - 1]) {
+					continue;
+				}
+				for (int j = i + 1; j < n - 2; j++) {
+					if (j > i + 1 && nums[j] == nums[j - 1]) {
+						continue;
+					}
+					List<List<Integer>> twins = twoSum(nums, (long) target - nums[i] - nums[j], j + 1, n - 1);
+					for (List<Integer> twin : twins) {
+						twin.add(nums[i]);
+						twin.add(nums[j]);
+						quadruplets.add(twin);
 					}
 				}
 			}
