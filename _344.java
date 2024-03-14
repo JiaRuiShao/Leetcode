7093925d4b4440944fd6784ac18a290cf554e66c
JiaRuiShao <jiaruishaojessie@gmail.com>
2024-03-14 10:48:22 -0500

    344. Reverse String

--- _344.java
+++ _344.java
@@ -7,17 +7,15 @@ public class _344 {
 	class Solution_Left_Right_Pointers {
 		public void reverseString(char[] s) {
 			int l = 0, r = s.length - 1;
-			while (l <= r) {
-				swap(s, l, r);
-				l++;
-				r--;
+			while (l < r) {
+				swap(s, l++, r--);
 			}
 		}
 		
-		private void swap(char[] s, int l, int r) {
-			char tmp = s[l];
-			s[l] = s[r];
-			s[r] = tmp;
+		private void swap(char[] arr, int l, int r) {
+			char rVal = arr[r];
+			arr[r] = arr[l];
+			arr[l] = rVal;
 		}
 	}
 }
