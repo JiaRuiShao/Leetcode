970ebfae6bc54c4192712b0e81b98209845e8817
JiaRuiShao <jiaruishaojessie@gmail.com>
2024-03-13 19:52:34 -0500

    1658. Minimum Operations to Reduce X to Zero

--- _1658.java
+++ _1658.java
@@ -36,5 +36,47 @@ public class _1658 {
 			return maxLen == Integer.MIN_VALUE ? -1 : n - maxLen;
 		}
 	}
+
+	/**
+	 * Here we have to check the corner case where target < 0 b/c x could be > sum of array and while (winSum > target) will always be true.
+	 * 
+	 * @param nums
+	 * @param x target sum to be removed
+	 * @return min num of left/right elems to be removed to met x target
+	 */
+	public int minOperations(int[] nums, int x) {
+        int target = Arrays.stream(nums).sum() - x;
+        if (target < 0) {
+			return -1;
+		} else if (target == 0) {
+			return nums.length;
+		}
+        
+        int left = 0, right = 0, winSum = 0, maxWinLen = -1;
+        while (right < nums.length) {
+            int add = nums[right++];
+            winSum += add;
+
+            while (winSum > target) {
+                int rem = nums[left++];
+                winSum -= rem;
+            }
+
+            if (winSum == target) {
+                maxWinLen = Math.max(maxWinLen, right - left);
+            }
+        }
+        return maxWinLen == -1 ? -1 : nums.length - maxWinLen;
+    }
+
+	public static void main(String[] args) {
+		// int[] nums = new int[]{1, 1};
+		// int minOperations = new _1658().minOperations(nums, 3);
+		// System.out.println(minOperations);
+
+		int[] nums = new int[]{8828,9581,49,9818,9974,9869,9991,10000,10000,10000,9999,9993,9904,8819,1231,6309};
+		int minOperations = new _1658().minOperations(nums, 134365);
+		System.out.println(minOperations);
+	}
 	
 }
