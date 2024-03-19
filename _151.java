a4ea5d56d00a3357fd94e6bb287e084fe0cfc63f
JiaRuiShao <jiaruishaojessie@gmail.com>
2024-03-18 22:29:06 -0500

    151. Reverse Words in a String

--- _151.java
+++ _151.java
@@ -50,4 +50,61 @@ public class _151 {
 		}
 		return i;
 	}
+
+	class Solution {
+		public String reverseWords(String s) {
+			char[] arr = s.toCharArray();
+			int len = arr.length;
+			reverse(arr, 0, len - 1);
+	
+			int left = 0, right = 0;
+			while (right < len) {
+				while (right < len && arr[right] != ' ') {
+					right++;
+				}
+				reverse(arr, left, right - 1);
+				while (right < len && arr[right] == ' ') {
+					right++;
+				}
+				left = right;
+			}
+			if (left + 1 < len) {
+				reverse(arr, left, len - 1);
+			}
+			
+			int end = removeDupSpace(arr);
+			return new String(arr).substring(0, end);
+		}
+	
+		private void reverse(char[] arr, int l, int r) {
+			while (l < r) {
+				char temp = arr[r];
+				arr[r] = arr[l];
+				arr[l] = temp;
+				l++;
+				r--;
+			}
+		}
+	
+		private int removeDupSpace(char[] arr) {
+			int len = arr.length;
+			int l = 0, r = 0;
+			while (r < len) {
+				while (r < len && arr[r] == ' ') {
+					r++;
+				}
+				if (l > 0 && r < len) {
+					arr[l++] = ' ';
+				}
+				while (r < len && arr[r] != ' ') {
+					arr[l++] = arr[r++];
+				}
+			}
+			return l;
+		}
+	}
+
+	public static void main(String[] args) {
+		System.out.println(new _151().new Solution().reverseWords("  hello world  "));
+	}
 }
