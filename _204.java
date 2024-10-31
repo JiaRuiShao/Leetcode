a49e6fb3eac9edcaa627934642fe4efd0169c1d3
JiaRuiShao <jiaruishaojessie@gmail.com>
2024-10-31 15:55:15 -0500

    204. Count Primes.

--- _204.java
+++ _204.java
@@ -49,28 +49,20 @@ public class _204 {
     }
 
     class Solution3_Sieve_of_Eratosthenes {
-        int countPrimes(int n) {
+        public int countPrimes(int n) {
+            int primeCount = 0;
             boolean[] isPrime = new boolean[n];
             Arrays.fill(isPrime, true);
-
-            for (int i = 2; i < n; i++) {
-                if (isPrime[i]) {
-                    for (int j = 2 * i; j < n; j += i) {
-                        isPrime[j] = false;
+            for (int num = 2; num < n; num++) {
+                if (isPrime[num]) {
+                    primeCount++;
+                    for (int multipleBy = 2; multipleBy * num < n; multipleBy++) {
+                        isPrime[multipleBy * num] = false;
                     }
                 }
             }
-
-            int count = 0;
-            for (int i = 2; i < n; i++) {
-                if (isPrime[i]) {
-                    count++;
-                }
-            }
-
-            return count;
+            return primeCount;
         }
-
     }
 
     class Solution4_Sieve_of_Eratosthenes_Improved {
@@ -93,7 +85,7 @@ public class _204 {
             }
 
             int count = 0;
-            for (int i = 2; i < n; i++) {
+            for (int i = 2; i < n; i++) { // start counting from num 2
                 if (isPrime[i]) {
                     count++;
                 }
