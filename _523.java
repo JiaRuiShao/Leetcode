d7d3025f956992e9947c7e881e49b9e500900c2a
JiaRuiShao <jiaruishaojessie@gmail.com>
2024-11-04 18:26:00 -0600

    523. Continuous Subarray Sum.

--- _523.java
+++ _523.java
@@ -15,19 +15,25 @@ public class _523 {
 	 */
 	class Solution1_PrefixSum_HashMap {
 		public boolean checkSubarraySum(int[] nums, int k) {
-			// (pre[i] - pre[j]) % k == 0 && i - j >= 2
-			// pre[i] % k == pre[j] % k , i - j >= 2
+			// (preSum[j] - preSum[i]) % k == 0
+			// preSum[j] % k == preSum[i] % k
+			// or preSum[j] - preSum[i] == 0
 			int n = nums.length;
-			int[] preSum = new int[n + 1];
+			int[] sum = new int[n + 1];
 			for (int i = 0; i < n; i++) {
-				preSum[i + 1] = preSum[i] + nums[i];
+				sum[i + 1] = sum[i] + nums[i];
 			}
-			Map<Integer, Integer> sumModulusK = new HashMap<>();
-			for (int i = 0; i < preSum.length; i++) {
-				int num = preSum[i] % k;
-				if (sumModulusK.containsKey(num)) {
-					if (i - sumModulusK.get(num) >= 2) return true;
-				} else sumModulusK.put(num, i);
+	
+			Map<Integer, Integer> moduloMap = new HashMap<>(); // save the modulo val
+			for (int i = 0; i <= n; i++) {
+				int modulo = sum[i] % k;
+				if (moduloMap.containsKey(modulo)) {
+					if (i - moduloMap.get(modulo) >= 2) {
+						return true;
+					}
+				} else {
+					moduloMap.put(modulo, i);
+				}
 			}
 			return false;
 		}
