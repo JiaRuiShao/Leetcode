4cb6e696ed8e3acffe5cdf6179dd09b7a01021d3
JiaRuiShao <jiaruishaojessie@gmail.com>
2025-02-04 09:54:21 -0600

    523. Continuous Subarray Sum

--- _523.java
+++ _523.java
@@ -3,10 +3,12 @@ import java.util.Map;
 
 /**
  * 523. Continuous Subarray Sum.
- * Given an integer array nums and an integer k, return true if nums has a continuous subarray of size
+ * Given an integer array nums and an integer k, return true if nums has a
+ * continuous subarray of size
  * at least two whose elements sum up to a multiple of k, or false otherwise.
  *
- * An integer x is a multiple of k if there exists an integer n such that x = n * k. 0 is always a multiple of k.
+ * An integer x is a multiple of k if there exists an integer n such that x = n
+ * * k. 0 is always a multiple of k.
  */
 public class _523 {
 	/**
@@ -23,7 +25,7 @@ public class _523 {
 			for (int i = 0; i < n; i++) {
 				sum[i + 1] = sum[i] + nums[i];
 			}
-	
+
 			Map<Integer, Integer> moduloMap = new HashMap<>(); // save the modulo val
 			for (int i = 0; i <= n; i++) {
 				int modulo = sum[i] % k;
@@ -37,21 +39,45 @@ public class _523 {
 			}
 			return false;
 		}
-		
+
+		// preSum idx starts with 0
 		public boolean checkSubarraySumImproved(int[] nums, int k) {
 			// (pre[i] - pre[j]) % k == 0 && i - j >= 2
 			// pre[i] % k == pre[j] % k , i - j >= 2
-			int n  = nums.length;
-			int num = 0;
+			int n = nums.length;
+			int preSum = 0;
 			Map<Integer, Integer> sumModulusK = new HashMap<>();
-			sumModulusK.put(num, 0);
+			sumModulusK.put(preSum, 0);
 			for (int i = 1; i <= n; i++) {
-				// preSuMapm % k = preSum[i]%k
-				num = (num + nums[i-1]) % k;
-				if (sumModulusK.containsKey(num)) {
-					if (i - sumModulusK.get(num) >= 2) return true;
+				preSum = (preSum + nums[i - 1]) % k;
+				if (sumModulusK.containsKey(preSum)) {
+					if (i - sumModulusK.get(preSum) >= 2)
+						return true;
+				} else {
+					sumModulusK.put(preSum, i);
 				}
-				else sumModulusK.put(num, i);
+			}
+			return false;
+		}
+
+		// preSum idx starts with -1
+		// RECOMMENDED
+		public boolean checkSubarraySumImproved2(int[] nums, int k) {
+			int preSum = 0;
+			Map<Integer, Integer> remainderIdx = new HashMap<>();
+			remainderIdx.put(preSum, -1);
+
+			for (int i = 0; i < nums.length; i++) {
+				preSum += nums[i];
+				int remainder = preSum % k;
+				if (remainderIdx.containsKey(remainder)) {
+					int prevIdx = remainderIdx.get(remainder);
+					if (i - prevIdx >= 2) {
+						return true;
+					}
+				} else {
+					remainderIdx.put(remainder, i);
+				}
 			}
 			return false;
 		}
