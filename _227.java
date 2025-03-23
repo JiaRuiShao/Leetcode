8a0fae61eb5f85de7d59567e8c42377e7a8c6ec9
JiaRuiShao <jiaruishaojessie@gmail.com>
2025-03-23 18:14:37 -0500

    227. Basic Calculator II

---
+++ _227.java
@@ -0,0 +1,94 @@
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * 227. Basic Calculator II
+ */
+public class _227 {
+    class Solution1_Without_Stack {
+        public int calculate(String s) {
+            int res = 0;
+            int curr = 0;
+            int last = 0;
+            char lastOpr = '+';
+
+            for (int pos = 0; pos < s.length(); pos++) {
+                char c = s.charAt(pos);
+                if (c >= '0' && c <= '9') {
+                    curr = curr * 10 + (c - '0');
+                }
+                if (isOperator(c) || pos + 1 == s.length()) {
+                    switch (lastOpr) {
+                        case '+':
+                            res += last;
+                            last = curr;
+                            break;
+                        case '-':
+                            res += last;
+                            last = -curr;
+                            break;
+                        case '*':
+                            last *= curr;
+                            break;
+                        case '/':
+                            last /= curr;
+                            break;
+                    }
+                    curr = 0;
+                    lastOpr = c;
+                }
+            }
+            return res + last;
+        }
+
+        private boolean isOperator(char c) {
+            return c == '+' || c == '-' || c == '*' || c == '/';
+        }
+    }
+
+    class Solution2_With_Stack {
+        public int calculate(String s) {
+            Deque<Integer> st = new LinkedList<>();
+
+            int num = 0;
+            char lastOpr = '+';
+
+            for (int i = 0; i < s.length(); i++) {
+                char c = s.charAt(i);
+
+                if (Character.isDigit(c)) {
+                    num = num * 10 + (c - '0');
+                }
+
+                if (isOperator(c) || i == s.length() - 1) {
+                    if (lastOpr == '+') st.push(num);
+                    else if (lastOpr == '-') st.push(-num);
+                    else if (lastOpr == '*') st.push(st.pop() * num);
+                    else if (lastOpr == '/') st.push(st.pop() / num);
+
+                    num = 0;
+                    lastOpr = c;
+                }
+            }
+
+            int ans = 0;
+
+            while (!st.isEmpty()) {
+                ans += st.pop();
+            }
+
+            return ans;
+        }
+
+        private boolean isOperator(char c) {
+            return c == '+' || c == '-' || c == '*' || c == '/';
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution1_Without_Stack solution = new _227().new Solution1_Without_Stack();
+        System.out.println(solution.calculate("3-2*2")); // -1
+        System.out.println(solution.calculate(" 3/2 ")); // 1.5
+        System.out.println(solution.calculate(" 3+5 / 2 ")); // 5.5
+    }
+}
