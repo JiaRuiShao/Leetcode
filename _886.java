e25acdba43ff7d4850aa0bee9f86379fa4ec0777
JiaRuiShao <jiaruishaojessie@gmail.com>
2025-03-25 08:55:17 -0500

    886. Possible Bipartition

--- _886.java
+++ _886.java
@@ -5,15 +5,15 @@ import java.util.Queue;
 
 /**
  * 886. Possible Bipartition
+ * Key Points:
+ * 1 - dislike pair [a, b] means a & b should be in different color, when construct graph, make them neighbors
+ * 2 - we need to build a undirected/bidirectional graph since the dislike is mutual
+ * 3 - node label is 1-based not 0-based in this question
  */
 public class _886 {
     class Solution1_BFS {
         /**
          * BFS Solution
-         * Key Points:
-         * 1 - dislike pair [a, b] means a & b should be in different color, when construct graph, make them neighbors
-         * 2 - we need to build a undirected/bidirectional graph since the dislike is mutual
-         * 3 - node label is 1-based not 0-based in this question
          * @param n nodes
          * @param dislikes dislikes
          * @return true if it's a valid bipartite graph
@@ -61,11 +61,10 @@ public class _886 {
         }
     }
 
+    /**
+     * BFS Implementation - Color when 1st traversal.
+     */
     class Solution2_DFS {
-        /**
-         * dislike pair [a, b] means a & b should be in different color
-         node label is 1-based
-         */
         public boolean possibleBipartition(int n, int[][] dislikes) {
             int[] color = new int[n]; // 1 , 2
             List<Integer>[] graph = new ArrayList[n];
@@ -97,6 +96,49 @@ public class _886 {
             color[node] = currColor;
             for (int neighbor : graph[node]) {
                 if (!dfs(neighbor, color, graph, 3 - currColor)) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+
+    /**
+     * DFS Implementation - Color before traversal.
+     */
+    class Solution3_DFS_Improved {
+        public boolean possibleBipartition(int n, int[][] dislikes) {
+            int[] colors = new int[n]; // 1 , 2
+            List<Integer>[] graph = new ArrayList[n];
+            for (int node = 0; node < n; node++) {
+                graph[node] = new ArrayList<>();
+            }
+
+            for (int[] edge : dislikes) {
+                // convert the node from 1-based to 0-based
+                graph[edge[0] - 1].add(edge[1] - 1);
+                graph[edge[1] - 1].add(edge[0] - 1);
+            }
+
+            for (int v = 0; v < n; v++) {
+                if (colors[v] == 0) {
+                    colors[v] = 1;
+                    if (!dfs(v, colors, graph)) {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        private boolean dfs(int v, int[] colors, List<Integer>[] graph) {
+            for (int nbr : graph[v]) {
+                if (colors[nbr] == 0) {
+                    colors[nbr] = 3 - colors[v];
+                    if (!dfs(nbr, colors, graph)) {
+                        return false;
+                    }
+                } else if (colors[nbr] == colors[v]) {
                     return false;
                 }
             }
