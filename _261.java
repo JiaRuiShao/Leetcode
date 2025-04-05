ff4ab599487ec9b9e76ac5ab217aeb4a3c7b32f9
JiaRuiShao <jiaruishaojessie@gmail.com>
2025-04-04 22:14:44 -0500

    261. Graph Valid Tree

--- _261.java
+++ _261.java
@@ -1,3 +1,36 @@
+/**
+ * 261. Graph Valid Tree
+ */
 public class _261 {
-
+    class Solution1_Union_Find {
+        // a valid tree: no cycle + all nodes connected
+        // T: O(n + e)
+        // S: O(n)
+        public boolean validTree(int n, int[][] edges) {
+            int connectedComponents = n;
+            int[] parent = new int[n];
+            for (int node = 0; node < n; node++) {
+                parent[node] = node;
+            }
+    
+            for (int[] edge : edges) {
+                int n1 = edge[0];
+                int n2 = edge[1];
+                int p1 = findParent(parent, n1);
+                int p2 = findParent(parent, n2);
+                if (p1 == p2) return false;
+                parent[p1] = p2;
+                connectedComponents--;
+            }
+    
+            return connectedComponents == 1;
+        }
+    
+        private int findParent(int[] parent, int node) {
+            if (parent[node] != node) {
+                parent[node] = findParent(parent, parent[node]);
+            }
+            return parent[node];
+        }
+    }
 }
