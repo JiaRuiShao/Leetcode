db04a2e4e2fbb3e1cf7ceb4db0ebe7b3e17bedd6
JiaRuiShao <jiaruishaojessie@gmail.com>
2025-04-06 16:48:57 -0500

    116. Populating Next Right Pointers in Each Node

--- _116.java
+++ _116.java
@@ -1,6 +1,11 @@
 import java.util.LinkedList;
 import java.util.Queue;
 
+/**
+ * 116. Populating Next Right Pointers in Each Node
+ * Solution 1: dfs traversal (constant space except for callstack)
+ * Solution 2: bfs (linear extra space)
+ */
 public class _116 {
 
     private static class Node {
@@ -21,75 +26,49 @@ public class _116 {
             right = _right;
             next = _next;
         }
-    };
+    }
 
-    static class SolutionBFS {
-
-        /**
-         * BFS Level-Order Traversal.
-         * Time: O(n)
-         * Space: O(n)
-         *
-         * @param root tree root node
-         * @return the root node
-         */
+    class Solution1_DFS {
         public Node connect(Node root) {
             if (root == null) return root;
-            Node curr, prev;
-            Queue<Node> q = new LinkedList<>();
-            q.offer(root);
-
-            while (!q.isEmpty()) {
-                int sz = q.size();
-                prev = null; //**IMPORTANT**
-
-                for (int i = 0; i < sz; i++) {
-                    curr = q.poll();
-
-                    // connect to left adjacent nodes if there's any
-                    if (prev != null) {
-                        prev.next = curr;
-                    }
-
-                    // add the left & right child if there's any
-                    if (curr.left != null) {
-                        q.offer(curr.left);
-                    }
-                    if (curr.right != null) {
-                        q.offer(curr.right);
-                    }
-
-                    // set this curr node as prev
-                    prev = curr;
-                }
-            }
-
+            dfs(root.left, root.right);
             return root;
+        }
+    
+        private void dfs(Node n1, Node n2) {
+            if (n1 == null || n2  == null) return;
+            n1.next = n2;
+            dfs(n1.left, n1.right);
+            dfs(n1.right, n2.left);
+            dfs(n2.left, n2.right);
         }
     }
 
-    static class SolutionRecursion {
-        Node connect(Node root) {
-            if (root == null) return null;
-            connectTwoNode(root.left, root.right);
-            return root;
-        }
-
-        void connectTwoNode(Node node1, Node node2) {
-            if (node1 == null || node2 == null) {
-                return;
+    class Solution2_BFS {
+        public Node connect(Node root) {
+            if (root == null) return root;
+    
+            Queue<Node> queue = new LinkedList<>();
+            queue.offer(root);
+            
+            while (!queue.isEmpty()) {
+                int sz = queue.size();
+                Node prev = null;
+                for (int i = 0; i < sz; i++) {
+                    Node curr = queue.poll();
+                    if (prev != null) {
+                        prev.next = curr;
+                    }
+                    if (curr.left != null) {
+                        queue.offer(curr.left);
+                    }
+                    if (curr.right != null) {
+                        queue.offer(curr.right);
+                    }
+                    prev = curr;
+                }
             }
-            /**** pre-order ****/
-            // connect the left & right child
-            node1.next = node2;
-
-            // recursively go to the left & right substree
-            connectTwoNode(node1.left, node1.right);
-            connectTwoNode(node2.left, node2.right);
-
-            /**** post-order ****/
-            // connect the nodes on the same level with different parent
-            connectTwoNode(node1.right, node2.left);
+            return root;
         }
     }
 }
