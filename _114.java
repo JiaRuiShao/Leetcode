8543176750c3a871de79bb71818172c409b220f6
JiaRuiShao <jiaruishaojessie@gmail.com>
2025-04-06 18:04:56 -0500

    114. Flatten Binary Tree to Linked List

--- _114.java
+++ _114.java
@@ -1,96 +1,81 @@
 import java.util.Stack;
 
+/**
+ * 114. Flatten Binary Tree to Linked List
+ */
 public class _114 {
-
-    private static class TreeNode {
-
-        int val;
-        TreeNode left;
-        TreeNode right;
-
-        TreeNode() {
+    // If we're allowed to return a new root node
+    class Solution0_DFS_Not_Work {
+        TreeNode curr;
+        public TreeNode flatten(TreeNode root) {
+            TreeNode dummyHead = new TreeNode();
+            curr = dummyHead;
+            dfs(root);
+            return dummyHead.right;
         }
-
-        TreeNode(int val) {
-            this.val = val;
-        }
-
-        TreeNode(int val, TreeNode left, TreeNode right) {
-            this.val = val;
-            this.left = left;
-            this.right = right;
+    
+        private void dfs(TreeNode node) {
+            if (node == null) return;
+            curr.right = new TreeNode(node.val);
+            curr = curr.right;
+            dfs(node.left);
+            dfs(node.right);
         }
     }
 
-    static class SolutionRecursion {
+    class Solution1_DFS {
 
-        /**
-         * Time: O(N)
-         * Space: O(H) where H is the height of the binary tree. It's O(logN) if the tree is a balanced tree; It's O(n) if else
-         */
-        void flatten(TreeNode root) {
-            // base case
-            if (root == null)
-                return;
+        TreeNode prev = null;
+    
+        public void flatten(TreeNode root) {
+            if (root == null) return;
+            TreeNode left = root.left, right = root.right;
+            if (prev != null) { // pre-order flatten
+                prev.right = root;
+                prev.left = null;
+            }
+            prev = root;
+            flatten(left);
+            flatten(right);
+        }
+    }
 
-            flatten(root.left);
-            flatten(root.right);
+    class Solution2_DFS_Without_Global_Variable {
+        public void flatten(TreeNode root) {
+            dfs(root, null);
+        }
+    
+        private TreeNode dfs(TreeNode node, TreeNode prev) {
+            if (node == null) return prev;
+            TreeNode left = node.left, right = node.right;
+            if (prev != null) { // pre-order flatten
+                prev.right = node;
+                prev.left = null;
+            }
+            prev = dfs(left, node);
+            return dfs(right, prev);
+        }
+    }
 
-            /**** post order ****/
+    class Solution3_Subproblem {
+        public void flatten(TreeNode root) {
+            if (root == null) return;
             TreeNode left = root.left;
             TreeNode right = root.right;
-
-            // make left subtree as right subtree
-            root.left = null;
-            root.right = left;
-
-            // add the original right subtree to the end of the original left subtree
-            while (root.right != null) {
-                root = root.right;
-            }
-
-            root.right = right;
-        }
-    }
-
-    static class SolutionRecursion2 {
-
-        /**
-         * Time: O(N)
-         * Space: O(H) where H is the height of the binary tree. It's O(logN) if the tree is a balanced tree; It's O(n) if else
-         * Ref: https://leetcode.com/problems/flatten-binary-tree-to-linked-list/solution/
-         */
-        TreeNode flat(TreeNode root) {
-            // base cases
-            if (root == null) return null;
-            if (root.left == null && root.right == null) return root;
-
-            // recursively flatten the left & right subtree
-            TreeNode leftTail = flat(root.left);
-            TreeNode rightTail = flat(root.right);
-
-            // post-order
-            if (leftTail != null) {
-                leftTail.right = root.right;
-                root.right = root.left;
+            flatten(root.left);
+            flatten(root.right);
+            if (left != null) {
+                root.right = left;
                 root.left = null;
+                while (left.right != null) {
+                    left = left.right;
+                }
+                left.right = right;
             }
-
-            // return the "rightmost" node 
-            return rightTail == null ? leftTail : rightTail;
-        }
-
-        void flatten(TreeNode root) {
-            flat(root);           
         }
     }
 
-    static class SolutionIterative {
-
-        /**
-         * Time: O(N)
-         * Space: O(1)
-         */
+    class Solution4_Iterative {
         void flatten(TreeNode root) {
             // base case
             if (root == null) return;
@@ -116,6 +101,26 @@ public class _114 {
                 // move on to the right side of the tree
                 node = node.right;
             }
+        }
+    }
+
+    private static class TreeNode {
+
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
         }
     }
 }
