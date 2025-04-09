cd3c4f0b3e52a989f4ec8d17986b4e1604719b71
JiaRuiShao <jiaruishaojessie@gmail.com>
2025-04-09 11:11:23 -0500

    378. Kth Smallest Element in a Sorted Matrix

--- _378.java
+++ _378.java
@@ -4,17 +4,70 @@ import java.util.List;
 import java.util.PriorityQueue;
 
 /**
- * 378. Kth Smallest Element in a Sorted Matrix.
+ * 378. Kth Smallest Element in a Sorted Matrix
  */
 public class _378 {
-    class Solution1_MinHeap {
+    class Solution1_Binary_Search {
         /**
-         * - Time: O(mlogm + klogm)
-         * - Space: O(m)
-         * 
-         * @param matrix matrix
-         * @param k      k
-         * @return kth smallest element
+         * Left boundary binary search + matrix traversal technique.
+         * We convert this problem to a binary search problem:
+         * Variable target num with search range as [matrix[0][0], matrix[m-1][n-1]]
+         * Function is num of elems in the matrix that <= target: f(x) = # of elem <= x in given matrix, monotonic increase
+         * Goal is to find the lower bound of variable num under the given function value k
+         *
+         * Time: O((m+n)log(2e9)) = O(nlog(2e9)) < O(30n) = O(n)
+         * Space: O(1)
+         */
+        public int kthSmallest(int[][] matrix, int k) {
+            int n = matrix.length; // in this question m == n
+            int left = matrix[0][0], right = matrix[n - 1][n - 1], mid = 0;
+            // 
+            // f(x) = # of elem <= x in given matrix, monotonic increase
+            while (left <= right) {
+                mid = left + (right - left) / 2;
+                int lessEqualCount = findLTECount(matrix, mid);
+                if (lessEqualCount >= k) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+            return left;
+        }
+
+        /**
+         * Find number of elements that are LTE to the given number x.
+         * We go diagonal from top-right to bottom-left or vice versa.
+         *
+         * Time: O(m + n) = O(2n) = O(n)
+         * Space: O(1)
+         */
+        private int findLTECount(int[][] mtx, int x) {
+            int n = mtx.length, r = n - 1, c = 0, count = 0;
+            while (r >= 0 && c < n) {
+                while (r >= 0 && mtx[r][c] > x) r--;
+                count += r + 1;
+                c++;
+            }
+            return count;
+        }
+
+        private int getLTECount(int[][] matrix, int target) {
+            int m = matrix.length, n = matrix[0].length;
+            int count = 0, r = 0, c = n - 1;
+            while (r < m && c >= 0) {
+                while (target < matrix[r][c]) c--;
+                count += c + 1;
+                r++;
+            }
+            return count;
+        }
+    }
+
+    class Solution2_MinHeap {
+        /**
+         * - Time: O(nlogn + klogn)
+         * - Space: O(n)
          */
         public int kthSmallest(int[][] matrix, int k) {
             int m = matrix.length, n = matrix[0].length;
@@ -34,67 +87,6 @@ public class _378 {
                 }
             }
             return kthSmallestNum;
-        }
-    }
-
-    class Solution2_Binary_Search {
-        /**
-         * Left boundary binary search + matrix traversal technique.
-         * We convert this problem to a binary search problem:
-         * variable target num with search range as [matrix[0][0], matrix[m-1][n-1]]
-         * function is num of elems in the matrix that <= target
-         * goal is to find the lower bound of variable num under the given function
-         * value k
-         *
-         * Time: O((m+n)log(2e9)) = O(nlog(2e9))
-         * Space: O(1)
-         * 
-         * @param matrix
-         * @param k
-         * @return
-         */
-        public int kthSmallest(int[][] matrix, int k) {
-            int n = matrix.length;
-            int left = matrix[0][0], right = matrix[n - 1][n - 1], mid = 0;
-            // search range: [left, right]
-            // f(x) = # of elem <= x in given matrix, monotonic increase
-            while (left <= right) {
-                mid = left + (right - left) / 2;
-                int lessEqualCount = findLessEqualCount(matrix, mid);
-                if (lessEqualCount >= k) {
-                    right = mid - 1;
-                } else {
-                    left = mid + 1;
-                }
-            }
-            return left;
-        }
-
-        /**
-         * Find number of elements that are smaller than or equal to the given number
-         * mid.
-         * We go left and down from top-right to bottom-left.
-         * This function has variable num (target) as x, & f(x) calculates the number of
-         * elements in this matrix that are
-         * smaller than or equal to the given x. This is a monotonic increasing function
-         * on variable num.
-         *
-         * Time: O(m + n) = O(2n) = O(n)
-         * Space: O(1)
-         *
-         * @param matrix matrix
-         * @param num    target
-         * @return # elem <= target in matrix
-         */
-        private int findLessEqualCount(int[][] mtx, int x) {
-            int n = mtx.length, r = n - 1, c = 0, count = 0;
-            while (r >= 0 && c < n) {
-                while (r >= 0 && mtx[r][c] > x)
-                    r--;
-                count += r + 1;
-                c++;
-            }
-            return count;
         }
     }
 
