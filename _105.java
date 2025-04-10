8af5062d69f3846fcd7016a4230358e0af945917
JiaRuiShao <jiaruishaojessie@gmail.com>
2025-04-10 12:18:54 -0500

    105. Construct Binary Tree from Preorder and Inorder Traversal

--- _105.java
+++ _105.java
@@ -10,25 +10,22 @@ public class _105 {
     class Solution {
         public TreeNode buildTree(int[] preorder, int[] inorder) {
             int n = inorder.length;
-            Map<Integer, Integer> nodeToIdx = new HashMap<>();
+            Map<Integer, Integer> inorderMap = new HashMap<>();
             for (int i = 0; i < n; i++) {
-                nodeToIdx.put(inorder[i], i);
+                inorderMap.put(inorder[i], i);
             }
-            return build(preorder, 0, n - 1, nodeToIdx, 0, n - 1);
+            return dfsBuild(preorder, 0, n - 1, inorderMap, 0, n - 1);
         }
-
-        private TreeNode build(int[] pre, int preLo, int preHi, Map<Integer, Integer> in, int inLo, int inHi) {
-            if (preLo > preHi || inLo > inHi) return null;
-            int val = pre[preLo];
-            TreeNode root = new TreeNode(val);
-
-            int inorderIdx = in.get(val);
-            int leftLength = inorderIdx - inLo;
-            int preLeft = preLo + 1;
-            int preRight = preLeft + leftLength - 1;
-
-            root.left = build(pre, preLeft, preRight, in, inLo, inorderIdx - 1);
-            root.right = build(pre, preRight + 1, preHi, in, inorderIdx + 1, inHi);
+    
+        private TreeNode dfsBuild(int[] preorder, int preL, int preR, Map<Integer, Integer> inorder, int inL, int inR) {
+            if (preL > preR || inL > inR) {
+                return null;
+            }
+            int rootVal = preorder[preL];
+            TreeNode root = new TreeNode(rootVal);
+            int leftLen = inorder.get(rootVal) - inL;
+            root.left = dfsBuild(preorder, preL + 1, preL + 1 + leftLen - 1, inorder, inL, inL + leftLen - 1);
+            root.right = dfsBuild(preorder, preL + 1 + leftLen - 1 + 1, preR, inorder, inL + leftLen + 1, inR);
             return root;
         }
     }
