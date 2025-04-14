eb282bf39436631bff0b4adec92d1dc412fe1732
JiaRuiShao <jiaruishaojessie@gmail.com>
2025-04-14 09:43:42 -0500

    90. Subsets II

--- _90.java
+++ _90.java
@@ -6,7 +6,7 @@ import java.util.List;
  * 90. Subsets II
  */
 public class _90 {
-    class Solution {
+    class Solution1 {
         public List<List<Integer>> subsetsWithDup(int[] nums) {
             List<List<Integer>> res = new ArrayList<>();
             List<Integer> selected = new ArrayList<>();
@@ -21,6 +21,29 @@ public class _90 {
                 int num = nums[select];
                 if (select > start && num == nums[select - 1]) continue; // ignore duplicate
                 selected.add(num);
+                backtrack(nums, select + 1, selected, res);
+                selected.remove(selected.size() - 1);
+            }
+        }
+    }
+
+    class Solution2 {
+        public List<List<Integer>> subsetsWithDup(int[] nums) {
+            List<List<Integer>> res = new ArrayList<>();
+            List<Integer> selected = new ArrayList<>();
+            Arrays.sort(nums);
+            backtrack(nums, 0, selected, res);
+            return res;
+        }
+    
+        private void backtrack(int[] nums, int start, List<Integer> selected, List<List<Integer>> res) {
+            res.add(new ArrayList<>(selected));
+            int prev = 11;
+            for (int select = start; select < nums.length; select++) {
+                int num = nums[select];
+                if (num == prev) continue; // ignore duplicate
+                selected.add(num);
+                prev = num;
                 backtrack(nums, select + 1, selected, res);
                 selected.remove(selected.size() - 1);
             }
