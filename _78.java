16dbe4d7f70140ea0589e800c6f3d6cb687c4ea2
JiaRuiShao <jiaruishaojessie@gmail.com>
2025-04-14 09:31:39 -0500

    78. Subsets

--- _78.java
+++ _78.java
@@ -10,19 +10,18 @@ public class _78 {
     class Solution1_Backtrack {
         public List<List<Integer>> subsets(int[] nums) {
             List<List<Integer>> res = new ArrayList<>();
-            Set<Integer> selected = new HashSet<>();
+            List<Integer> selected = new ArrayList<>();
             backtrack(nums, selected, res, 0);
             return res;
         }
-
-        private void backtrack(int[] nums, Set<Integer> selected, List<List<Integer>> res, int start) {
+    
+        private void backtrack(int[] nums, List<Integer> selected, List<List<Integer>> res, int start) {
             res.add(new ArrayList<>(selected));
-            for (int i = start; i < nums.length; i++) {
-                int num = nums[i];
-                if (selected.contains(num)) continue;
+            for (int select = start; select < nums.length; select++) {
+                int num = nums[select];
                 selected.add(num);
-                backtrack(nums, selected, res, i + 1);
-                selected.remove(num);
+                backtrack(nums, selected, res, select + 1);
+                selected.remove(selected.size() - 1);
             }
         }
     }
