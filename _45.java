1e89b803f52f532920a5202d18a7da0554a8c317
JiaRuiShao <jiaruishaojessie@gmail.com>
2025-04-14 14:30:08 -0500

    45. Jump Game II

---
+++ _45.java
@@ -0,0 +1,56 @@
+/**
+ * 45. Jump Game II
+ */
+public class _45 {
+    class Solution1_Greedy {
+        public int jump(int[] nums) {
+            int n = nums.length, steps = 0, reach = 0, jump = 0;
+            for (int i = 0; i < n - 1; i++) {
+                jump = Math.max(jump, i + nums[i]);
+                if (reach == i) {
+                    steps++;
+                    reach = jump; // jump points the farthest idx this step can reach
+                }
+            }
+            return steps;
+        }
+    }
+
+    class Solution2_DP_Iterative_Bottom_Up_With_Memo { // Skip the DP top-down recursive solution here
+        public int jump(int[] nums) {
+            int n = nums.length;
+            int[] steps = new int[n];
+            steps[n - 1] = 0;
+            for (int start = n - 2; start >= 0; start--) {
+                int reach = Math.min(start + nums[start], n - 1);
+                int minStep = n;
+                for (int jump = start + 1; jump <= reach; jump++) {
+                    if (steps[jump] < minStep) {
+                        minStep = steps[jump];
+                    }
+                }
+                steps[start] = minStep + 1;
+            }
+            return steps[0];
+        }
+    }
+
+    class Wrong_Answer_Greedy_Update {
+        // if it's not guaranteed to jump to n - 1
+        // Q: why not work? input: [7,0,9,6,9,6,1,7,9,0,1,2,9,0,3]; output should be 2, but below code return 4
+        public int jump(int[] nums) {
+            int n = nums.length, jump = 0, step = 0;
+            for (int pos = 0; pos < n - 1; pos++) {
+                int currJump = pos + nums[pos];
+                if (jump < n - 1 && currJump > jump) {
+                    jump = currJump;
+                    step++;
+                }
+                if (jump <= pos) {
+                    break;
+                }
+            }
+            return jump >= n - 1 ? step : -1;
+        }
+    }
+}
