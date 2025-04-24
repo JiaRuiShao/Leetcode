53116e4547c7a3a155cbd5f333465a3ea475e9ec
JiaRuiShao <jiaruishaojessie@gmail.com>
2025-04-23 23:29:33 -0500

    3. Longest Substring Without Repeating Characters

--- _3.java
+++ _3.java
@@ -1,33 +1,50 @@
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 /**
- * 3. Longest Substring Without Repeating Characters.
+ * 3. Longest Substring Without Repeating Characters
  */
 public class _3 {
-    class Solution1 {
+    class Solution1_HashMap {
         public int lengthOfLongestSubstring(String s) {
-            Map<Character, Integer> freq = new HashMap<>();
-            int left = 0, right = 0, invalid = 0, maxLen = 0; // window: [left, right)
+            Map<Character, Integer> charFreq = new HashMap<>();
+            int left = 0, right = 0, maxWinLen = 0;
             while (right < s.length()) {
-                char c = s.charAt(right);
-                right++;
-                freq.put(c, freq.getOrDefault(c, 0) + 1);
-                if (freq.get(c) == 2) invalid++;
-                while (invalid > 0) {
-                    c = s.charAt(left);
-                    left++;
-                    freq.put(c, freq.get(c) - 1);
-                    if (freq.get(c) == 1) invalid--;
+                char toAdd = s.charAt(right++);
+                charFreq.put(toAdd, charFreq.getOrDefault(toAdd, 0) + 1);
+                while (charFreq.get(toAdd) > 1) {
+                    char toRemove = s.charAt(left++);
+                    charFreq.put(toRemove, charFreq.get(toRemove) - 1);
                 }
-                maxLen = Math.max(maxLen, right - left);
+                maxWinLen = Math.max(maxWinLen, right - left);
             }
-            return maxLen;
+            return maxWinLen;
+        }
+    }
+
+    class Solution2_HashSet {
+        public int lengthOfLongestSubstring(String s) {
+            Set<Character> winChars = new HashSet<>();
+            int left = 0, right = 0, maxWin = 0;
+            while (right < s.length()) {
+                char add = s.charAt(right++);
+                while (winChars.contains(add)) {
+                    char rem = s.charAt(left++);
+                    winChars.remove(rem);
+                }
+                maxWin = Math.max(maxWin, right - left);
+                winChars.add(add);
+            }
+            return maxWin;
         }
     }
     
-    class Solution2 {
+    class Solution3_Array {
+        
         private static final int MAX_CHAR = 256;
+
         public int lengthOfLongestSubstring(String s) {
             int[] count = new int[MAX_CHAR];
             int left = 0, right = 0, maxLen = 0;
