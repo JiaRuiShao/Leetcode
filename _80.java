8f2fcfa3a1f9f9ce5074231554c16aae80467e5b
JiaRuiShao <jiaruishaojessie@gmail.com>
2025-04-28 10:38:07 -0500

    80. Remove Duplicates from Sorted Array II

--- _80.java
+++ _80.java
@@ -4,18 +4,15 @@
 public class _80 {
     class Solution1_Two_Pointers {
         public int removeDuplicates(int[] nums) {
-            // range [0, left)
-            int left = 0, right = 0;
-            while (right < nums.length) {
-                int distinct = nums[right];
+            int n = nums.length, left = 0, right = 0;
+            while (right < n) {
+                int distinct = nums[right++];
                 int start = right;
-
                 nums[left++] = distinct;
-                while (right < nums.length && nums[right] == distinct) {
+                while (right < n && nums[right] == distinct) {
                     right++;
                 }
-                // extra step: check freq of this distinct num > 1
-                if (right > start + 1) {
+                if (right > start) {
                     nums[left++] = distinct;
                 }
             }
