2b1ccf4bb9f81f84f9580fb49d2d05e73f6ab968
JiaRuiShao <jiaruishaojessie@gmail.com>
2025-05-01 12:40:51 -0500

    15. 3Sum

--- _15.java
+++ _15.java
@@ -1,9 +1,14 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 /**
- * 15. 3Sum.
+ * 15. 3Sum
  */
 public class _15 {
 
@@ -84,6 +89,31 @@ public class _15 {
 				}
 			}
 			return triplets;
+		}
+	}
+
+	class Solution2_Sorting_Not_Allowed {
+		public List<List<Integer>> threeSum(int[] nums) {
+			Set<List<Integer>> res = new HashSet<>();
+			Set<Integer> dups = new HashSet<>();
+			Map<Integer, Integer> seen = new HashMap<>();
+			for (int i = 0; i < nums.length; ++i) {
+				if (!dups.add(nums[i])) continue;
+				for (int j = i + 1; j < nums.length; ++j) {
+					int complement = -nums[i] - nums[j];
+					if (seen.containsKey(complement) && seen.get(complement) == i) {
+						List<Integer> triplet = Arrays.asList(
+							nums[i],
+							nums[j],
+							complement
+						);
+						Collections.sort(triplet);
+						res.add(triplet);
+					}
+					seen.put(nums[j], i);
+				}
+			}
+			return new ArrayList(res);
 		}
 	}
 }
