5059e545d0c7b98be57ab0cd24dd696afc64003d
Jessie Shao <jiaruis@andrew.cmu.edu>
2025-05-26 08:44:32 -0700

    503. Next Greater Element II

--- _503.java
+++ _503.java
@@ -1,13 +1,14 @@
 import java.util.ArrayDeque;
+import java.util.Arrays;
 import java.util.Deque;
 
 /**
- * 503. Next Greater Element II.
+ * 503. Next Greater Element II
  * Traverse two times to make sure we get all NGE for all elements.
  * Use modulo to get the actual index.
  */
 public class _503 {
-    class Solution {
+    class Solution1_Mono_Stack {
         /**
          * Monotonic Increasing Stack.
          * Time: O(n)
@@ -18,17 +19,35 @@ public class _503 {
          */
         public int[] nextGreaterElements(int[] nums) {
             int n = nums.length;
-            int[] res = new int[n];
-            Deque<Integer> stack = new ArrayDeque<Integer>(n);
+            int[] nge = new int[n];
+            Deque<Integer> stack = new ArrayDeque<>(n);
             for (int i = 2 * n - 1; i >= 0; i--) {
                 int idx = i % n;
                 while (!stack.isEmpty() && stack.peek() <= nums[idx]) {
                     stack.pop();
                 }
-                res[idx] = stack.isEmpty() ? -1 : stack.peek();
+                nge[idx] = stack.isEmpty() ? -1 : stack.peek();
                 stack.push(nums[idx]);
             }
-            return res;
+            return nge;
+        }
+    }
+    
+    class Solution2_Mono_Stack {
+        public int[] nextGreaterElements(int[] nums) {
+            int n = nums.length;
+            int[] nge = new int[n];
+            Arrays.fill(nge, -1);
+            Deque<Integer> stk = new ArrayDeque<>();
+            for (int i = 2 * n - 1; i >= 0; i--) {
+                int idx = i % n, curr = nums[idx];
+                while (!stk.isEmpty() && stk.peekFirst() <= curr) {
+                    stk.removeFirst();
+                }
+                if (!stk.isEmpty()) nge[idx] = stk.peekFirst();
+                stk.addFirst(curr);
+            }
+            return nge;
         }
     }
 }
