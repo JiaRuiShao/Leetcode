59c696f9d6f6b0b9bb8ac62fbffb18b4a1a764ee
Jessie Shao <jiaruis@andrew.cmu.edu>
2025-05-26 18:27:51 -0700

    81. Search in Rotated Sorted Array II

--- _81.java
+++ _81.java
@@ -1,31 +1,37 @@
 /**
- * 81. Search in Rotated Sorted Array II.
+ * 81. Search in Rotated Sorted Array II
  */
 public class _81 {
-	public boolean search(int[] nums, int target) {
-		int left = 0, right = nums.length - 1;
-		while (left <= right) {
-			while (left + 1 < nums.length && nums[left] == nums[left + 1]) left++;
-			while (right - 1 >= 0 && nums[right] == nums[right - 1]) right--;
-			if (left > right)
-				return nums[left] == target;
-			int mid = left + (right - left) / 2;
-			if (nums[mid] == target) {
-				return true;
-			} else if (nums[left] <= nums[mid]) { // on left side, [left, mid] sorted
-				if (nums[left] <= target && target < nums[mid]) { // go left
-					right = mid - 1;
-				} else { // go right
-					left = mid + 1;
+	class Solution1_Binary_Search {
+		public boolean search(int[] nums, int target) {
+			int lo = 0, hi = nums.length - 1;
+			while (lo <= hi) {
+				int mid = lo + (hi - lo) / 2;
+				if (nums[mid] == target) return true;
+				
+				// If we can't tell which half is sorted because of duplicates
+				if (nums[lo] == nums[mid] && nums[mid] == nums[hi]) {
+					lo++;
+					hi--;
 				}
-			} else { // on right side, [mid, right] sorted
-				if (nums[mid] < target && target <= nums[right]) { // go right
-					left = mid + 1;
-				} else { // go left
-					right = mid - 1;
+				// Left half is sorted
+				else if (nums[lo] <= nums[mid]) {
+					if (target >= nums[lo] && target < nums[mid]) {
+						hi = mid - 1;
+					} else {
+						lo = mid + 1;
+					}
+				}
+				// Right half is sorted
+				else {
+					if (target > nums[mid] && target <= nums[hi]) {
+						lo = mid + 1;
+					} else {
+						hi = mid - 1;
+					}
 				}
 			}
+			return false;
 		}
-		return false;
 	}
 }
