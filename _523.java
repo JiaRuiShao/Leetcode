87dd45a2f013da09b69cdf57a93afdecccb07bac
Jessie Shao <jiaruis@andrew.cmu.edu>
2025-06-16 07:45:24 -0700

    523. Continuous Subarray Sum

--- _523.java
+++ _523.java
@@ -2,40 +2,31 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * 523. Continuous Subarray Sum.
+ * 523. Continuous Subarray Sum
  * Given an integer array nums and an integer k, return true if nums has a
  * continuous subarray of size
  * at least two whose elements sum up to a multiple of k, or false otherwise.
  *
  * An integer x is a multiple of k if there exists an integer n such that x = n
- * * k. 0 is always a multiple of k.
+ * k. 0 is always a multiple of k.
  */
 public class _523 {
 	/**
 	 * Time: O(n)
 	 * Space: O(n)
+	 * (preSum[j] - preSum[i]) % k == 0 ==> preSum[j] % k == preSum[i] % k
 	 */
 	class Solution1_PrefixSum_HashMap {
 		public boolean checkSubarraySum(int[] nums, int k) {
-			// (preSum[j] - preSum[i]) % k == 0
-			// preSum[j] % k == preSum[i] % k
-			// or preSum[j] - preSum[i] == 0
-			int n = nums.length;
-			int[] sum = new int[n + 1];
-			for (int i = 0; i < n; i++) {
-				sum[i + 1] = sum[i] + nums[i];
-			}
-
-			Map<Integer, Integer> moduloMap = new HashMap<>(); // save the modulo val
-			for (int i = 0; i <= n; i++) {
-				int modulo = sum[i] % k;
-				if (moduloMap.containsKey(modulo)) {
-					if (i - moduloMap.get(modulo) >= 2) {
-						return true;
-					}
-				} else {
-					moduloMap.put(modulo, i);
+			int sum = 0;
+			Map<Integer, Integer> sumModKToIdx = new HashMap<>();
+			for (int i = 0; i <= nums.length; i++) {
+				if (i > 0) sum += nums[i - 1];
+				int mod = sum % k;
+				if (sumModKToIdx.containsKey(mod) && i >= sumModKToIdx.get(mod) + 2) {
+					return true;
 				}
+				sumModKToIdx.putIfAbsent(mod, i);
 			}
 			return false;
 		}
