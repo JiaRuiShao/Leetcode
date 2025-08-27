2d6fe7f9d3d67737310882c83d15f7151bcfd4c2
JiaRuiShao <jiaruishaojessie@gmail.com>
2025-08-27 11:07:15 -0700

    784. Letter Case Permutation

--- _784.java
+++ _784.java
@@ -5,9 +5,9 @@ import java.util.List;
  * 784. Letter Case Permutation
  */
 public class _784 {
-    class Solution1_Backtrack_Combination_ElemUsedOnce_NoDedup {
-        // Time: O(2^n)
-        // Space: O(n)
+    class Solution1_Backtrack_By_Char_Combination_ElemUsedOnce_NoDedup {
+        // Time: O(n*2^L) where n is s len and L is letter num
+        // Space: auxiliary O(n) total O(n*2^L)
         public List<String> letterCasePermutation(String s) {
             List<String> strings = new ArrayList<>();
             backtrack(s, strings, new StringBuilder(), 0);
@@ -39,4 +39,25 @@ public class _784 {
             return Character.toUpperCase(c);
         }
     }
+
+    class Solution2_Iterative {
+        // Time: O(n*2^L) where n is s len and L is letter num
+        // Space: auxiliary O(n) total O(n*2^L)
+        public List<String> letterCasePermutation(String s) {
+            List<String> res = new ArrayList<>();
+            res.add(s);
+            for (int i = 0; i < s.length(); i++) { // for each char
+                if (!Character.isLetter(s.charAt(i))) continue;
+                int size = res.size();
+                for (int j = 0; j < size; j++) { // for each prev generated permutation
+                    char[] t = res.get(j).toCharArray();
+                    t[i] = Character.isLowerCase(t[i]) ? Character.toUpperCase(t[i])
+                                                    : Character.toLowerCase(t[i]);
+                    res.add(new String(t));
+                }
+            }
+            return res;
+        }
+    }
+
 }
