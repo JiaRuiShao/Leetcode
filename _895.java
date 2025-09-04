4c0536f64b3e021c5e030569b0d81c959694f2e9
JiaRuiShao <jiaruishaojessie@gmail.com>
2025-09-03 20:52:53 -0700

    895. Maximum Frequency Stack

--- _895.java
+++ _895.java
@@ -7,41 +7,38 @@ import java.util.Map;
  * 895. Maximum Frequency Stack
  */
 public class _895 {
-    /**
-     * This solution uses two maps—one for tracking element frequencies and another for mapping frequencies to stacks.
-     */
-    class FreqStack {
+    class Solution1_HashMap_Stack {
+        class FreqStack {
+            Map<Integer, Integer> freq;
+            Map<Integer, Deque<Integer>> freqStk;
+            int maxFreq;
 
-        Map<Integer, Integer> freq;
-        Map<Integer, Deque<Integer>> freqStk;
-        int maxFreq;
-
-        public FreqStack() {
-            freq = new HashMap<>();
-            freqStk = new HashMap<>();
-            maxFreq = 0;
-        }
-
-        public void push(int val) {
-            freq.put(val, freq.getOrDefault(val, 0) + 1);
-            int valFreq = freq.get(val);
-            freqStk.putIfAbsent(valFreq, new LinkedList<>());
-            freqStk.get(valFreq).push(val);
-            maxFreq = Math.max(maxFreq, valFreq);
-        }
-
-        public int pop() {
-            Deque<Integer> maxFreqStk = freqStk.get(maxFreq);
-            int remove = maxFreqStk.pop();
-            if (maxFreqStk.isEmpty()) {
-                freqStk.remove(maxFreq);
-                maxFreq--;
-                // test will fail if write like:
-                // freqStk.remove(maxFreq--);
+            public FreqStack() {
+                freq = new HashMap<>();
+                freqStk = new HashMap<>();
+                maxFreq = 0;
             }
-            freq.put(remove, freq.get(remove) - 1);
-            return remove;
+
+            public void push(int val) {
+                freq.put(val, freq.getOrDefault(val, 0) + 1);
+                int valFreq = freq.get(val);
+                freqStk.putIfAbsent(valFreq, new LinkedList<>());
+                freqStk.get(valFreq).push(val);
+                maxFreq = Math.max(maxFreq, valFreq);
+            }
+
+            public int pop() {
+                Deque<Integer> maxFreqStk = freqStk.get(maxFreq);
+                int remove = maxFreqStk.pop();
+                if (maxFreqStk.isEmpty()) {
+                    freqStk.remove(maxFreq);
+                    maxFreq--;
+                    // test will fail if write like:
+                    // freqStk.remove(maxFreq--);
+                }
+                freq.put(remove, freq.get(remove) - 1);
+                return remove;
+            }
         }
     }
-
 }
