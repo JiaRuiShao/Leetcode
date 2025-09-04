ffefeab8d2719226b18a392260b0a3bbe29ac426
JiaRuiShao <jiaruishaojessie@gmail.com>
2025-09-03 22:48:10 -0700

    496. Next Greater Element I

--- _496.java
+++ _496.java
@@ -1,74 +1,61 @@
+import java.util.ArrayDeque;
 import java.util.Deque;
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.Map;
 
 /**
- * 496. Next Greater Element I.
+ * 496. Next Greater Element I
  */
 public class _496 {
-    /**
-     * Monotonic Stack & HashMap.
-     * Time: O(n + m) = O(m) where n is the length of subset nums1 and m is the length of the array nums2
-     * Space: O(2m) = O(m) where m is length of nums2
-     */
-    class Solution1 {
-        private int[] findNGLMappings(int[] nums, Map<Integer, Integer> map, int[] ngl) {
-            int[] res = new int[nums.length];
-            for (int i = 0; i < nums.length; i++) {
-                int idxInNums2 = map.get(nums[i]);
-                res[i] = ngl[idxInNums2];
+    class Solution1_BruteForce {
+        public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+            int[] res = new int[nums1.length];
+            for (int i = 0; i < nums1.length; i++) {
+                int num = nums1[i];
+                int pos = 0;
+                for (int j = 0; j < nums2.length; j++) {
+                    if (nums2[j] == num) {
+                        pos = j;
+                        break;
+                    }
+                }
+                int ngl = -1;
+                for (int j = pos + 1; j < nums2.length; j++) {
+                    if (nums2[j] > num) {
+                        ngl = nums2[j];
+                        break;
+                    }
+                }
+                res[i] = ngl;
             }
             return res;
         }
+    }
 
-        private int[] findNextGreaterElem(int[] nums, Map<Integer, Integer> map) {
-            int n = nums.length;
-            int[] ngl = new int[n];
-            Deque<Integer> stack = new LinkedList<>();
-            for (int i = n - 1; i >= 0; i--) {
-                int num = nums[i];
-                map.put(num, i);
-
-                while (!stack.isEmpty() && stack.peek() <= num) {
-                    stack.pop();
-                }
-
-                ngl[i] = stack.isEmpty() ? -1 : stack.peek();
-                stack.push(num);
+    // Use monotonic stack to get the next greater elements for each number in O(n) time complexity
+    class Solution2_MonoStack {
+        public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+            Map<Integer, Integer> valToNGL = findNGL(nums2);
+            int[] ngl = new int[nums1.length];
+            for (int i = 0; i < nums1.length; i++) {
+                ngl[i] = valToNGL.get(nums1[i]);
             }
-
             return ngl;
         }
 
-        public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-            Map<Integer, Integer> valueIdxMap = new HashMap<>();
-            int[] ngl = findNextGreaterElem(nums2, valueIdxMap);
-            return findNGLMappings(nums1, valueIdxMap, ngl);
-        }
-    }
-
-    class Solution2 {
-        /**
-         * Use monotonic stack to get the next greater elements for each number in O(n) time complexity.
-         */
-        public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-            Deque<Integer> stack = new LinkedList<>();
+        private Map<Integer, Integer> findNGL(int[] nums) {
             Map<Integer, Integer> map = new HashMap<>();
-            for (int i = nums2.length - 1; i >= 0; i--) {
-                while (!stack.isEmpty() && stack.peekLast() < nums2[i]) {
-                    stack.pollLast(); // poll the elements that are less than the current elem who are on the right side
+            Deque<Integer> maxStk = new ArrayDeque<>();
+            for (int i = nums.length - 1; i >= 0; i--) {
+                int num = nums[i];
+                while (!maxStk.isEmpty() && maxStk.peek() <= num) {
+                    maxStk.pop();
                 }
-                int nge = stack.isEmpty() ? -1 : stack.peekLast(); // next greater element for nums2[i]
-                map.put(nums2[i], nge);
-                stack.addLast(nums2[i]);
+                Integer ngl = maxStk.peek();
+                map.put(num, ngl == null ? -1 : ngl);
+                maxStk.push(num);
             }
-
-            int n = nums1.length;
-            for (int i = 0; i < n; i++) {
-                nums1[i] = map.get(nums1[i]);
-            }
-            return nums1;
+            return map;
         }
     }
 }
