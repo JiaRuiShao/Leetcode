190a0b6c9939f18ea3f2ab6d48a99da975cd80a3
JiaRuiShao <jiaruishaojessie@gmail.com>
2025-09-06 22:57:25 -0700

    1019. Next Greater Node In Linked List

--- _1019.java
+++ _1019.java
@@ -1,24 +1,17 @@
 import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Deque;
+import java.util.List;
+import java.util.Stack;
 
 import helper.ListNode;
 
 /**
- * 1019. Next Greater Node In Linked List.
+ * 1019. Next Greater Node In Linked List
  */
 public class _1019 {
-    /**
-     * Definition for singly-linked list.
-     * public class ListNode {
-     *     int val;
-     *     ListNode next;
-     *     ListNode() {}
-     *     ListNode(int val) { this.val = val; }
-     *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-     * }
-     */
-    class Solution {
+    // find ngl from right to left; update ngl for current idx after popped out smaller elements in maxStack
+    class Solution1_MonoStack {
         public int[] nextLargerNodes(ListNode head) {
             ArrayList<Integer> list = new ArrayList<>();
             ListNode curr = head;
@@ -39,6 +32,32 @@ public class _1019 {
                 stack.push(val);
             }
             return ans;
+        }
+    }
+
+    // when we find ngl from left to right; we need to store the idx and update the ngl for that idx when that idx got popped out
+    class Solution2_MonoStack_OnePass {
+        public int[] nextLargerNodes(ListNode head) {
+            List<Integer> answer = new ArrayList<>();
+            Stack<int[]> stack = new Stack<>();
+            // We use an integer 'cnt' to represent the index.
+            int cnt = 0;
+
+            while (head != null) {
+                // Set the next greater value of the current value 'head.val' as 0 by default.
+                answer.add(0);
+                while (!stack.isEmpty() && head.val > stack.peek()[1]) {
+                    int[] curr = stack.peek();
+                    int id = curr[0], val = curr[1];
+                    stack.pop();
+                    answer.set(id, head.val);
+                }
+                // Add both the index and the value to stack.
+                stack.add(new int[]{cnt++, head.val});
+                head = head.next;
+            }
+        
+            return answer.stream().mapToInt(i -> i).toArray();
         }
     }
 }
