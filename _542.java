eed8d70eef046888a7ed9622d225c00b5e15d56a
JiaRuiShao <jiaruishaojessie@gmail.com>
2025-09-19 15:50:39 -0700

    542. 01 Matrix

--- _542.java
+++ _542.java
@@ -1,56 +1,42 @@
-import java.util.Arrays;
-import java.util.LinkedList;
+import java.util.ArrayDeque;
 import java.util.Queue;
 
+/**
+ * 542. 01 Matrix
+ */
 public class _542 {
-    static class Solution {
+    class Solution1_BFS {
+        // notice starting from 1 would lead to O((mn)^2) time whereas starting from 0 is only O(mn) time
+        int[][] dirs = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
         public int[][] updateMatrix(int[][] mat) {
-            int m = mat.length;
-            int n = mat[0].length;
-            int[][] ans = new int[m][n];
-            Queue<int[]> q = new LinkedList<>(); // maintain the queue of cells to be examined next
-
-            for (int i = 0; i < m; i++) {
-                for (int j = 0; j < n; j++) {
-                    if (mat[i][j] == 0) {
-                        q.offer(new int[]{i, j}); // add all the cells with 0s to q
-                    } else {
-                        ans[i][j] = Integer.MAX_VALUE; // need to be updated during the BFS
+            int m = mat.length, n = mat[0].length;
+            Queue<int[]> q = new ArrayDeque<>();
+            boolean[][] visited = new boolean[m][n];
+            for (int r = 0; r < m; r++) {
+                for (int c = 0; c < n; c++) {
+                    if (mat[r][c] == 0) {
+                        q.offer(new int[]{r, c});
                     }
                 }
             }
-
-            int[] directions = new int[]{0, 1, 0, -1, 0}; // (x, y+1), (x+1, y), (x, y-1), (x-1, 0)
-            while (!q.isEmpty()) { // BFS start
-                int[] curr = q.poll(); // poll the cell from queue
-                int x = curr[0];
-                int y = curr[1];
-
-                for (int i = 0; i < directions.length - 1; i++) { // examine its neighbors
-                    int nextX = directions[i] + curr[0];
-                    int nextY = directions[i + 1] + curr[1];
-
-                    // if the new calculated distance for neighbor (x,y) is smaller, we add (x,y) to q and update dist[x][y]
-                    if (nextX >= 0 && nextX < m && nextY >= 0 && nextY < n && ans[nextX][nextY] > ans[x][y] + 1) {
-                        q.offer(new int[]{nextX, nextY});
-                        ans[nextX][nextY] = ans[x][y] + 1;
+            int step = 0;
+            while (!q.isEmpty()) {
+                int size = q.size();
+                step++;
+                for (int i = 0; i < size; i++) {
+                    int[] cell = q.poll();
+                    int r = cell[0], c = cell[1];
+                    for (int[] dir : dirs) {
+                        int nr = r + dir[0], nc = c + dir[1];
+                        if (nr >= 0 && nr < m && nc >= 0 && nc < n && mat[nr][nc] == 1 && !visited[nr][nc]) {
+                            q.offer(new int[]{nr, nc});
+                            mat[nr][nc] = step;
+                            visited[nr][nc] = true;
+                        }
                     }
                 }
             }
-            return ans;
+            return mat;
         }
     }
-
-    public static void main(String[] args) {
-        int[][] mat = new int[][] {{0,0,0}, {0,1,0}, {0,0,0}};
-        System.out.println(Arrays.deepToString(new Solution().updateMatrix(mat)));
-
-        // [[0,0,0],[0,1,0],[0,0,0]]
-        System.out.println();
-
-        mat = new int[][] {{0,0,0}, {0,1,0}, {1,1,1}};
-        System.out.println(Arrays.deepToString(new Solution().updateMatrix(mat)));
-        // [[0,0,0],[0,1,0],[1,2,1]]
-    }
-    
 }
