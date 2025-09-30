868086c5c50e1868a1c61f85771e29f7927e8b55
JiaRuiShao <jiaruishaojessie@gmail.com>
2025-09-29 21:44:05 -0700

    127. Word Ladder

--- _127.java
+++ _127.java
@@ -1,108 +1,61 @@
-import java.util.Arrays;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 import java.util.Set;
 
+/**
+ * 127. Word Ladder
+ */
 public class _127 {
-
-    /**
-     * Time: O(N + (M * 24) * N) = O(N + 240N) = O(N) where N is the number of words in the given word list (dictionary)
-     * - 1 ≤ M ≤ 10 according to the requirement
-     * - at most N newWord
-     * 
-     * Space: O(3N + N * M + N * M * 23) = O(3N + 240N) = O(N)
-     * - 1≤ M ≤ 10 according to the requirement
-     * - char[] chars: O(N * M)
-     * - String newWord: O(N * M * 23)
-     */
-    static class BFS {
-
-        private void addNextWords(String word, Queue<String> words, Set<String> dict, Set<String> visited) {
-            char[] chars = word.toCharArray();
-            for (int i = 0; i < chars.length; i++) {
-                char prev = chars[i]; // record the original char
-
-                for (char c = 'a'; c <= 'z'; c++) {
-                    if (c == prev) continue;
-                    chars[i] = c;
-                    String newWord = new String(chars);
-                    if (dict.contains(newWord) && !visited.contains(newWord)) {
-                        words.offer(newWord);
-                        visited.add(newWord);
-                    }
-                }
-
-                chars[i] = prev; // change the char back
-            }
-            
-        }
-
+    class Solution1_BFS {
         public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-            Queue<String> words = new LinkedList<>();
+            Set<String> whitelist = new HashSet<>();
+            for (String word : wordList) whitelist.add(word);
+            if (!whitelist.contains(endWord)) return 0;
+
+            Queue<String> q = new ArrayDeque<>();
             Set<String> visited = new HashSet<>();
-            Set<String> dict = new HashSet<>(wordList);
-            int step = 1;
-
-            words.offer(beginWord);
-            while (!words.isEmpty()) {
-                int sz = words.size();
-                for (int i = 0; i < sz; i++) {
-                    String word = words.poll();
-                    if (word.equalsIgnoreCase(endWord)) return step;
-                    addNextWords(word, words, dict, visited);
-                }
-                step++;
-            }
-
-            return 0;
-        }
-    }
-
-    static class BidirectionalBFS {
-
-        public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-            Set<String> begin = new HashSet<>();
-            Set<String> end = new HashSet<>();
-            Set<String> visited = new HashSet<>();
-            Set<String> dict = new HashSet<>(wordList); // **Important**
-    
-            int step = 1;
-            begin.add(beginWord);
-            if (dict.contains(endWord)) end.add(endWord);
+            q.offer(beginWord);
             visited.add(beginWord);
-            visited.add(endWord);
-    
-            while (!begin.isEmpty() && !end.isEmpty()) {
-                Set<String> nextBegin = new HashSet<>();
-                for (String word : begin) {
-                    char[] chars = word.toCharArray();
-                    for (int i = 0; i < chars.length; i++) {
-                        char prev = chars[i]; // record the original char
-                        for (char c = 'a'; c <= 'z'; c++) {
-                            if (c == prev) continue;
-                            chars[i] = c;
-                            String newWord = new String(chars);
-                            if (end.contains(newWord)) return step + 1;
-                            if (dict.contains(newWord) && !visited.contains(newWord)) {
-                                nextBegin.add(newWord);
-                                visited.add(newWord);
-                            }
+
+            int transform = 0;
+            while (!q.isEmpty()) {
+                int size = q.size();
+                transform++;
+                for (int i = 0; i < size; i++) {
+                    String word = q.poll();
+                    if (word.equals(endWord)) return transform;
+                    for (String nextWord : getNextWords(word)) {
+                        if (whitelist.contains(nextWord) && !visited.contains(nextWord)) {
+                            q.offer(nextWord);
+                            visited.add(nextWord);
                         }
-                        chars[i] = prev; // change the char back
                     }
                 }
-                step++;
-                begin = end;
-                end = nextBegin;
             }
-    
             return 0;
+        }
+
+        // we can use either char[] or StringBuilder here
+        private List<String> getNextWords(String word) {
+            List<String> words = new ArrayList<>();
+            char[] arr = word.toCharArray();
+            for (int i = 0; i < arr.length; i++) {
+                char oldLetter = arr[i];
+                for (char letter = 'a'; letter <= 'z'; letter++) {
+                    if (letter == oldLetter) continue;
+                    arr[i] = letter;
+                    words.add(new String(arr));
+                }
+                arr[i] = oldLetter;
+            }
+            return words;
         }
     }
 
-    static class BidirectionalBFSImproved {
+    class Solution1_BidirectionalBFS {
 
         public int ladderLength(String beginWord, String endWord, List<String> wordList) {
             Set<String> begin = new HashSet<>();
@@ -148,17 +101,5 @@ public class _127 {
     
             return 0;
         }
-    }
-
-    public static void main(String[] args) {
-        String beginWord = "hit";
-        String endWord = "cog";
-        List<String> wordList = Arrays.asList("hot","dot","dog","lot","log","cog");
-        System.out.println(new BFS().ladderLength(beginWord, endWord, wordList));
-        System.out.println(new BidirectionalBFS().ladderLength(beginWord, endWord, wordList));
-
-        wordList = Arrays.asList("hot","dot","dog","lot","log");
-        System.out.println(new BFS().ladderLength(beginWord, endWord, wordList));
-        System.out.println(new BidirectionalBFS().ladderLength(beginWord, endWord, wordList));
     }
 }
