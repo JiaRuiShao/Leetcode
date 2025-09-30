7631728e2d56b0b058bc44e8516da2df783c9ec6
JiaRuiShao <jiaruishaojessie@gmail.com>
2025-09-29 21:48:20 -0700

    658. Find K Closest Elements

--- _658.java
+++ _658.java
@@ -2,63 +2,12 @@ import java.util.LinkedList;
 import java.util.List;
 
 /**
- * 658. Find K Closest Elements.
+ * 658. Find K Closest Elements
  * 1. Left Boundary BS
  * 2. Initialize the two pointers start range with an empty range
  */
 public class _658 {
-	class Solution1_Binary_Search_Left_Right_Pointers {
-		/**
-		 * Time: O(logn + k) where n is the number of elements in given arr
-		 * Space: O(1)
-		 * 
-		 * @param arr elements arr
-		 * @param k k nearest elements
-		 * @param x target value
-		 * @return closest k elements near x
-		 */
-		public List<Integer> findClosestElements(int[] arr, int k, int x) {
-			// binary search first to find the left boundary of x
-			// then use two pointers to go from the left boundary to left & right until k elements added
-			return findKElems(arr, k, x, findLowerBound(arr, x));
-		}
-		
-		// find the target index and return
-		// if there's more than one target elements, return the left most index
-		// if the target doesn't exist in the given arr, return the index i of the largest arr[i] < target
-		private int findLowerBound(int[] arr, int target) {
-			int left = 0, right = arr.length - 1;
-			while (left <= right) {
-				int mid = left + (right - left) / 2;
-				if (arr[mid] == target) {
-					right = mid - 1;
-				} else if (arr[mid] < target) {
-					left = mid + 1;
-				} else if (arr[mid] > target) {
-					right = mid - 1;
-				}
-			}
-			return left;
-		}
-		
-		private List<Integer> findKElems(int[] arr, int k, int x, int start) {
-			// use a LinkedList to implement so that addFirst() is O(1) time complexity
-			LinkedList<Integer> kSmallestElems = new LinkedList<>();
-			// **IMPORTANT: start idx could be arr.length, so left has to be initialized as start - 1**
-			int left = start - 1, right = start;
-			while (k > 0) { // or while (kSmallestElems.size() < k)
-				if (right == arr.length || (left >= 0 && Math.abs(arr[left] - x) <= Math.abs(arr[right] - x))) {
-					kSmallestElems.addFirst(arr[left--]);
-				} else {
-					kSmallestElems.addLast(arr[right++]);
-				}
-				k--;
-			}
-			return kSmallestElems;
-		}
-	}
-
-	class Solution2_Binary_Search_Improved {
+	class Solution1_Binary_Search { // lower bound BS
 		public List<Integer> findClosestElements(int[] arr, int k, int x) {
 			int xIdx = findLeftX(arr, x);
 			return findKClosestElements(arr, k, x, xIdx);
@@ -92,6 +41,27 @@ public class _658 {
 				}
 			}
 			return res;
+		}
+	}
+	
+	class Solution2_Binary_Search { // right/upper boundary BS
+		public List<Integer> findClosestElements(int[] arr, int k, int x) {
+			int n = arr.length;
+			int lo = 0, hi = n - 1, mid;
+			while (lo <= hi) {
+				mid = lo + (hi - lo) / 2;
+				if (arr[mid] <= x) lo = mid + 1; // go right
+				else if (arr[mid] > x) hi = mid - 1; // go left
+			}
+			List<Integer> kClosest = new LinkedList<>();
+			int left = hi, right = lo;
+			while (kClosest.size() < k) {
+				if (left < 0) kClosest.add(arr[right++]); // addLast
+				else if (right >= n) kClosest.add(0, arr[left--]); // addFirst
+				else if (Math.abs(arr[right] - x) < Math.abs(arr[left] - x)) kClosest.add(arr[right++]);
+				else kClosest.add(0, arr[left--]);
+			}
+			return kClosest;
 		}
 	}
 }
