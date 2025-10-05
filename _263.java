b9e53a371624e8a1c607df4dbde5a481afae2224
JiaRuiShao <jiaruishaojessie@gmail.com>
2025-10-04 21:01:18 -0700

    263. Ugly Number

--- _263.java
+++ _263.java
@@ -1,7 +1,5 @@
 /**
- * 263. Ugly Number.
- * An ugly number is a positive integer whose prime factors are limited to 2, 3, and 5.
- * Given an integer n, return true if n is an ugly number.
+ * 263. Ugly Number
  */
 public class _263 {
 	/**
