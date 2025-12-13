b0577f0f5f63cc6314ff90495760ba34cac592c1
JiaRuiShao <jiaruishaojessie@gmail.com>
2025-12-13 15:14:14 -0800

    700. Search in a Binary Search Tree

--- _700.java
+++ _700.java
@@ -1,11 +1,13 @@
 import helper.TreeNode;
 
+/**
+ * 700. Search in a Binary Search Tree
+ */
 public class _700 {
     class Solution {
         public TreeNode searchBST(TreeNode root, int val) {
             if (root == null) return null;
             if (root.val == val) return root;
-
             return root.val < val ? searchBST(root.right, val) : searchBST(root.left, val);
         }
     }
