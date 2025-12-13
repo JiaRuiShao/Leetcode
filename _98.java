dfa9c770de2e8c7881dbbf0a22756f757edaeb34
JiaRuiShao <jiaruishaojessie@gmail.com>
2025-12-13 15:31:37 -0800

    98. Validate Binary Search Tree

--- _98.java
+++ _98.java
@@ -4,7 +4,8 @@ import java.util.ArrayDeque;
 import java.util.Deque;
 
 /**
- * 98. Validate Binary Search Tree.
+ * 98. Validate Binary Search Tree
+ * 
  * Given the root of a binary tree, determine if it is a valid binary search tree (BST).
  * A valid BST is defined as follows:
  *  - The left subtree of a node contains only nodes with keys less than the node's key.
