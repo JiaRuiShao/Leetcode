348a6f5c5e035dc9a560fd315d72e880888a67b2
JiaRuiShao <jiaruishaojessie@gmail.com>
2025-12-16 11:09:15 -0800

    42. Trapping Rain Water

--- _42.java
+++ _42.java
@@ -1,7 +1,11 @@
 /**
  * 42. Trapping Rain Water
+ * For bar i, water it can trap: max(0, min(max(height[0...i-1], height[i...n-1])) - height[i])
  */
 public class _42 {
+    // Solution1: brute force improved, Time: O(n), Space: O(n)
+    // use pre-calculated maxLeft[i] & maxRight[i] to eliminate the inner for loop in brute force approach
+    // here we could also use mono stack to find next taller bar
     class Solution1_Prefix_Suffix {
         public int trap(int[] height) {
             int n = height.length;
@@ -24,22 +28,24 @@ public class _42 {
         }
     }
 
-    // uses two pointers to tracks the maximum heights from both sides
+    // Solution2: greedy two pointers to tracks the maximum heights from both ends, Time: O(n), Space: O(1)
     // calculate trapped water by moving the side with the smaller maximum height inward
     class Solution2_Two_Pointers {
         public int trap(int[] height) {
-            int left = 0, right = height.length - 1;
-            int maxLeft = 0, maxRight = 0, total = 0;
-            while (left < right) {
-                maxLeft = Math.max(maxLeft, height[left]);
-                maxRight = Math.max(maxRight, height[right]);
-                if (maxLeft < maxRight) {
-                    total += maxLeft - height[left++];
+            int left = 0, right = height.length - 1, leftMaxHeight = 0, rightMaxHeight = 0, totalTrapped = 0;
+            while (left <= right) {
+                int leftHeight = height[left], rightHeight = height[right];
+                leftMaxHeight = Math.max(leftHeight, leftMaxHeight);
+                rightMaxHeight = Math.max(rightHeight, rightMaxHeight);
+                if (leftMaxHeight < rightMaxHeight) {
+                    totalTrapped += Math.max(0, leftMaxHeight - height[left++]);
+                    // left++;
                 } else {
-                    total += maxRight - height[right--];
+                    totalTrapped += Math.max(0, rightMaxHeight - height[right--]);
+                    // right--;
                 }
             }
-            return total;
+            return totalTrapped;
         }
     }
 }
