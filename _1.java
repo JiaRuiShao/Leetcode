6914e67ce9bad40cb29f425b336673f0236c167e
JiaRuiShao <jiaruishaojessie@gmail.com>
2025-12-18 15:50:07 -0800

    1. Two Sum

--- _1.java
+++ _1.java
@@ -1,11 +1,35 @@
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
  * 1. Two Sum
+ * 
+ *  1 - BF: nested for loop O(n^2), O(1)
+ *  2 - Sort + TP O(nlogn), O(n)
+ *  3 - Map<Val, Idx> O(n), O(n)
+ * 
+ * Clarification:
+ * - One solution?
+ * - Each element used once?
+ * - Return value or indices?
+ * - Does order matter?
+ * 
+ * Followup:
+ * - Input is sorted? Use two pointers with O(n) time and O(1) space
+ * - What if you can't use extra space (no HashMap)? Brute Force O(n^2); Sort-in place + two pointers O(nlogn) still need extra O(n) space to store val to idx mappings unless the input nums is guaranteed to be sorted
+ * - Multiple solutions: return all pairs
+ * - Input is a stream? Store limited size of val, idx pairs; remove oldest when full
+ * - Extend to Three Sum? LC 15 must sort for three sum
+ * 
+ * Red Flags:
+ * - Use same elem twice
+ * - Return val instead of indices
+ * - Missing duplicate checking
  */
 public class _1 {
     // assumptions: cannot use the same element twice
@@ -34,20 +58,36 @@ public class _1 {
         // Time: O(n)
         // Space: O(n)
         public int[] twoSum(int[] nums, int target) {
-            if (nums == null || nums.length < 2) return new int[0];
-        
-            Map<Integer, Integer> numToIndex = new HashMap<>();
+            int[] indices = new int[2];
+            Map<Integer, Integer> valToIdx = new HashMap<>();
             for (int i = 0; i < nums.length; i++) {
-                int complement = target - nums[i];
-                Integer complementIndex = numToIndex.get(complement);
-                if (complementIndex != null) {
-                    return new int[]{complementIndex, i};
+                int num = nums[i], supplement = target - num;
+                if (valToIdx.containsKey(supplement)) {
+                    indices[0] = valToIdx.get(supplement);
+                    indices[1] = i;
+                    break;
                 }
-                numToIndex.put(nums[i], i);
+                valToIdx.putIfAbsent(num, i);
             }
-            return new int[0];
+            return indices;
         }
-        
+    }
+
+    static class FollowupMultipleAnswers {
+        public List<int[]> twoSums(int[] nums, int target) {
+            List<int[]> pairs = new ArrayList<>();
+            Map<Integer, List<Integer>> valToIndices = new HashMap<>();
+            for (int i = 0; i < nums.length; i++) {
+                int num = nums[i], supplement = target - num;
+                if (valToIndices.containsKey(supplement)) {
+                    for (int idx : valToIndices.get(supplement)) {
+                        pairs.add(new int[]{idx, i});
+                    }
+                }
+                valToIndices.computeIfAbsent(num, k -> new ArrayList<>()).add(i);
+            }
+            return pairs;
+        }
     }
 
     @Test
