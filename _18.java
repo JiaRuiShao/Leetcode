e7bd56ab08289ebc84107fabfab4303e5931e268
JiaRuiShao <jiaruishaojessie@gmail.com>
2025-12-19 12:33:55 -0800

    18. 4Sum

--- _18.java
+++ _18.java
@@ -3,31 +3,48 @@ import java.util.Arrays;
 import java.util.List;
 
 /**
- * 18. 4Sum.
- * Integer Overflow as 3 * 10^9 > 2^31 - 1
+ * 18. 4Sum
+ * 
+ * 1 - BF O(n^4), O(1)
+ * 2 - Set O(n^3), O(n^4)
+ * 3 - Sort + TF O(n^3), O(logn)
+ * 
+ * Clarification:
+ * - Value range? -10⁹ to 10⁹, target can be -10⁹ to 10⁹
+ * - Does output order matters?
+ * - Is the array sorted?
+ * 
+ * Red Flags:
+ * - Didn't recognize Integer Overflow as 3 * 10^9 > 2^31 - 1
+ * - Wrong deduplication logic
  */
 public class _18 {
-	class Solution1 {
+	// Time: O(n^3)
+	// Space: O(logn) dual-pivot sorting call stack height
+	class Solution1_Sort_TwoPointers {
 		public List<List<Integer>> fourSum(int[] nums, int target) {
-			int n = nums.length;
 			Arrays.sort(nums);
 			List<List<Integer>> quadruplets = new ArrayList<>();
-			for (int a = 0; a < n - 3; a++) {
-				if (a > 0 && nums[a] == nums[a - 1]) continue;
-				for (int b = a + 1; b < n - 2; b++) {
-					if (b > a + 1 && nums[b] == nums[b - 1]) continue;
-					int c = b + 1, d = n - 1;
-					while (c < d) {
-						int left = nums[c], right = nums[d];
-						long sum = (long) left + right, key = (long) target - nums[a] - nums[b];
-						if (sum == key) {
-							quadruplets.add(Arrays.asList(nums[a], nums[b], left, right));
-							while (c < d && nums[c] == left) c++;
-							while (c < d && nums[d] == right) d--;
-						} else if (sum < key) {
-							c++;
+			int n = nums.length;
+			for (int i = 0; i < n - 3; i++) {
+				if (i > 0 && nums[i] == nums[i - 1]) continue;
+				for (int j = i + 1; j < n - 2; j++) {
+					if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+					// long k = target - nums[i] - nums[j]; // overflow before cast and save as long
+					long k = (long) target - nums[i] - nums[j];
+					int left = j + 1, right = n - 1;
+					while (left < right) {
+						long sum = nums[left] + nums[right];
+						if (sum == k) {
+							quadruplets.add(List.of(nums[i], nums[j], nums[left], nums[right]));
+							while (left + 1 < n && nums[left + 1] == nums[left]) left++;
+							while (right - 1 > j && nums[right - 1] == nums[right]) right--;
+							left++;
+							right--;
+						} else if (sum < k) {
+							left++;
 						} else {
-							d--;
+							right--;
 						}
 					}
 				}
@@ -36,55 +53,47 @@ public class _18 {
 		}
 	}
 
-	class Solution2 {
+	class Followup_kSum_Sort_Backtrack_TwoPointers {
+		public List<List<Integer>> kSum(int[] nums, int target, int k) {
+			Arrays.sort(nums);
+			List<List<Integer>> result = new ArrayList<>();
+    		kSumHelper(nums, target, k, 0, new ArrayList<>(), result);
+			return result;
+		}
 
-		private List<List<Integer>> twoSum(int[] nums, long target, int left, int right) {
-			List<List<Integer>> twins = new ArrayList<>();
-			while (left < right) {
-				int leftVal = nums[left];
-				int rightVal = nums[right];
-				long cpr = leftVal + rightVal - target;
-				if (cpr == 0) {
-					List<Integer> twin = new ArrayList<>();
-					twin.add(leftVal);
-					twin.add(rightVal);
-					twins.add(twin);
-					while (left < right && nums[left] == leftVal) left++;
-					while (left < right && nums[right] == rightVal) right--;
-				} else if (cpr > 0) {
-					while (left < right && nums[right] == rightVal) right--;
-				} else {
-					while (left < right && nums[left] == leftVal) left++;
+		private void kSumHelper(int[] nums, long target, int k, int start, 
+                        List<Integer> current, List<List<Integer>> result) {			
+			if (k == 2) {
+				// Base case: two-sum with two pointers
+				int left = start, right = nums.length - 1;
+				while (left < right) {
+					long sum = (long) nums[left] + nums[right];
+					if (sum == target) {
+						List<Integer> combination = new ArrayList<>(current);
+						combination.add(nums[left]);
+						combination.add(nums[right]);
+						result.add(combination);
+
+						while (left < right && nums[left] == nums[left + 1]) left++;
+						while (left < right && nums[right] == nums[right - 1]) right--;
+						left++;
+						right--;
+					} else if (sum < target) {
+						left++;
+					} else {
+						right--;
+					}
 				}
+				return;
 			}
 			
-			return twins;
-		}
-	
-		public List<List<Integer>> fourSum(int[] nums, int target) {
-			int n = nums.length;
-			List<List<Integer>> quadruplets = new ArrayList<>();
-			if (n < 4) {
-				return quadruplets;
+			// Recursive case
+			for (int i = start; i < nums.length - k + 1; i++) {
+				if (i > start && nums[i] == nums[i - 1]) continue;
+				current.add(nums[i]);
+				kSumHelper(nums, target - nums[i], k - 1, i + 1, current, result);
+				current.remove(current.size() - 1);  // Backtrack
 			}
-			Arrays.sort(nums);
-			for (int i = 0; i < n - 3; i++) {
-				if (i > 0 && nums[i] == nums[i - 1]) {
-					continue;
-				}
-				for (int j = i + 1; j < n - 2; j++) {
-					if (j > i + 1 && nums[j] == nums[j - 1]) {
-						continue;
-					}
-					List<List<Integer>> twins = twoSum(nums, (long) target - nums[i] - nums[j], j + 1, n - 1);
-					for (List<Integer> twin : twins) {
-						twin.add(nums[i]);
-						twin.add(nums[j]);
-						quadruplets.add(twin);
-					}
-				}
-			}
-			return quadruplets;
 		}
 	}
 }
