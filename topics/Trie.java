7612b2a2ac4866e46f5b3c647dc0259ea039b71e
JiaRuiShao <jiaruishaojessie@gmail.com>
2025-12-24 13:23:07 -0800

    Trie

---
+++ topics/Trie.java
@@ -0,0 +1,32 @@
+package topics;
+
+public class Trie {
+    class TrieNode {
+        TrieNode[] children = new TrieNode[26];
+        boolean isEndOfWord;
+    }
+
+    private TrieNode root = new TrieNode();
+
+    public void insert(String word) {
+        TrieNode node = root;
+        for (char c : word.toCharArray()) {
+            int idx = c - 'a';
+            if (node.children[idx] == null) {
+                node.children[idx] = new TrieNode();
+            }
+            node = node.children[idx];
+        }
+        node.isEndOfWord = true;
+    }
+
+    public boolean search(String word) {
+        TrieNode node = root;
+        for (char c : word.toCharArray()) {
+            int idx = c - 'a';
+            if (node.children[idx] == null) return false;
+            node = node.children[idx];
+        }
+        return node.isEndOfWord;
+    }
+}
