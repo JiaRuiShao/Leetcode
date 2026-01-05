89be9ace394e6d87b53cfcd060f63c0f1d4adf12
JiaRuiShao <jiaruishaojessie@gmail.com>
2026-01-04 16:01:48 -0800

    123. Best Time to Buy and Sell Stock III

--- _123.java
+++ _123.java
@@ -1,39 +1,81 @@
 /**
  * 123. Best Time to Buy and Sell Stock III
+ * 
+ * - S1: BF O(n^2), O(1)
+ * - S2: Greedy two pass with prefix & suffix profit array O(n), O(n)
+ * - S3: DP O(n), O(n)/O(1)
+ * 
+ * Clarification:
+ * - Can I buy & sell on the same day?
+ * - Can I assume prices are non-negative?
  */
 public class _123 {
-    class Solution1_DP {
+    class Solution1_Greedy_TwoPass_SuffixPrefix {
+        // prefix profit & suffix profit
         public int maxProfit(int[] prices) {
             int n = prices.length;
-            int[][][] dp =  new int[n + 1][2][2];
-            // base cases
-            dp[0][0][0] = 0;
-            dp[0][0][1] = -100001;
-            dp[0][1][0] = 0;
-            dp[0][1][1] = -100001;
-            // transitions
-            for (int day = 0; day < n; day++) {
-                dp[day + 1][0][0] = Math.max(dp[day][0][0], dp[day][0][1] + prices[day]);
-                dp[day + 1][0][1] = Math.max(dp[day][0][1], - prices[day]);
-                dp[day + 1][1][0] = Math.max(dp[day][1][0], dp[day][1][1] + prices[day]);
-                dp[day + 1][1][1] = Math.max(dp[day][1][1], dp[day][0][0] - prices[day]);
+            int[] prefix = new int[n]; // max profit from [0, i]
+            int[] suffix = new int[n]; // max profit from [i, n-1]
+
+            // max(price[j] - price[i]) ==> given price[j], find min(price[i]) where i < j
+            int minPrice = prices[0], maxProfit = 0;
+            for (int i = 0; i < n; i++) {
+                int price = prices[i];
+                maxProfit = Math.max(maxProfit, price - minPrice);
+                prefix[i] = maxProfit;
+                minPrice = Math.min(minPrice, price);
             }
-            return dp[n][1][0];
+
+            // max(price[j] - price[i]) ==> given price[i], find max(price[j]) where i < j
+            int maxPrice = prices[n - 1];
+            maxProfit = 0;
+            for (int i = n - 1; i >= 0; i--) {
+                int price = prices[i];
+                maxProfit = Math.max(maxProfit, maxPrice - price);
+                suffix[i] = maxProfit;
+                maxPrice = Math.max(maxPrice, price);
+            }
+
+            maxProfit = 0;
+            for (int i = 0; i < n; i++) {
+                maxProfit = Math.max(maxProfit, prefix[i] + suffix[i]); // notice here we assume we CAN sell & buy at same day; however here this requirement doesn't change result
+            }
+            return maxProfit;
         }
     }
 
-    class Solution2_DP_Without_Memo {
+    class Solution1_DP {
         public int maxProfit(int[] prices) {
-            int notHoldT1 = 0, holdT1 = -100001, notHoldT2 = 0, holdT2 = -100001;
+            int n = prices.length;
+            // dp[i][k][0/1] = max profit when not hold/hold a stock with at most k-1 transactions on day i-1
+            // i = day, k = hold at most k transactions, 0/1 = not holding/holding
+            int[][][] dp = new int[n+1][3][2];
+            for (int k = 0; k <= 2; k++) {
+                dp[0][k][0] = 0;
+                dp[0][k][1] = -prices[0] - 1; // not yet bought, Integer.MIN_VALUE / 2 is preferred here
+            }
+
+            for (int i = 1; i <= n; i++) {
+                int price = prices[i - 1];
+                for (int k = 1; k <= 2; k++) {
+                    dp[i][k][0] = Math.max(dp[i-1][k][0], dp[i-1][k][1] + price);
+                    dp[i][k][1] = Math.max(dp[i-1][k][1], dp[i-1][k-1][0] - price);
+                }
+            }
+            return dp[n][2][0];
+        }
+    }
+
+    class Solution2_DP_SpaceOptimized {
+        public int maxProfit(int[] prices) {
+            int notHoldT1 = 0, notHoldT2 = 0, holdT1 = -prices[0] - 1, holdT2 = -prices[0] - 1;
             for (int price : prices) {
-                // dp[day + 1][0][0] = Math.max(dp[day][0][0], dp[day][0][1] + prices[day]);
+                int temp = notHoldT1; // yesterday's max profit not holding a stock with at most 1 transaction
+                // here we can use today's notHoldT1 to calc today's holdT2, whether we can sell & buy at same day doesn't matter for the result, but it's good to know the semantic difference
                 notHoldT1 = Math.max(notHoldT1, holdT1 + price);
-                // dp[day + 1][0][1] = Math.max(dp[day][0][1], - prices[day]);
                 holdT1 = Math.max(holdT1, -price);
-                // dp[day + 1][1][0] = Math.max(dp[day][1][0], dp[day][1][1] + prices[day]);
                 notHoldT2 = Math.max(notHoldT2, holdT2 + price);
-                // dp[day + 1][1][1] = Math.max(dp[day][1][1], dp[day][0][0] - prices[day]);
-                holdT2 = Math.max(holdT2, notHoldT1 - price);
+                holdT2 = Math.max(holdT2, temp - price);
             }
             return notHoldT2;
         }
