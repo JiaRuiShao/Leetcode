2c9ebbc9c497e0237500f3ac4abc070d2a9c5b81
JiaRuiShao <jiaruishaojessie@gmail.com>
2026-01-04 16:54:25 -0800

    309. Best Time to Buy and Sell Stock with Cooldown

--- _309.java
+++ _309.java
@@ -1,5 +1,8 @@
 /**
  * 309. Best Time to Buy and Sell Stock with Cooldown
+ * 
+ * Followup:
+ * - What if cooldown is d days instead of 1? Need cooldown[] to check notHold maxProfit for i-d-1 day
  */
 public class _309 {
     class Solution0_DFS_Brute_Force {
@@ -28,23 +31,18 @@ public class _309 {
     class Solution1_DP {
         public int maxProfit(int[] prices) {
             int n = prices.length;
-            int[][] dp = new int[n + 1][2];
-            dp[0][0] = 0;
-            dp[0][1] = -1001; // -(max stock price + 1)
-            for (int i = 0; i < n; i++) {
-                int day = i + 1;
-                dp[day][0] = Math.max(dp[i][0], dp[i][1] + prices[i]);
-                if (i - 1 < 0) { // prevent array index out of bound error
-                    dp[day][1] = Math.max(dp[i][1], dp[i][0] - prices[i]);
-                } else {
-                    dp[day][1] = Math.max(dp[i][1], dp[i - 1][0] - prices[i]);
-                }
+            int[][] dp = new int[n+1][2];
+            dp[0][1] = Integer.MIN_VALUE / 2;
+            for (int i = 1; i <= n; i++) {
+                int price = prices[i-1], prevSellIdx = Math.max(0, i-2);
+                dp[i][0] = Math.max(dp[i-1][0], dp[i-1][1] + price);
+                dp[i][1] = Math.max(dp[i-1][1], dp[prevSellIdx][0] - price);
             }
             return dp[n][0];
         }
     }
 
-    class Solution2_DP_Without_Memo {
+    class Solution2_DP_SpaceOptimized {
         public int maxProfit(int[] prices) {
             int notHold = 0, prevNotHold = 0, hold = Integer.MIN_VALUE; // Equivalent to -infinity or -1001
     
@@ -55,6 +53,22 @@ public class _309 {
                 prevNotHold = temp;
             }
     
+            return notHold;
+        }
+    }
+    
+    // We can use a queue with size as d+1 to reduce space complexity to O(d)
+    class Followup_CooldownDDays {
+        public int maxProfit(int[] prices, int d) {
+            int n = prices.length;
+            int notHold = 0, hold = Integer.MIN_VALUE / 2;
+            int[] cooldown = new int[n];
+            for (int i = 0; i < n; i++) {
+                int price = prices[i], cooldownIdx = Math.max(0, i - d - 1);
+                notHold = Math.max(notHold, hold + price);
+                hold = Math.max(hold, cooldown[cooldownIdx] - price);
+                cooldown[i] = notHold;
+            }
             return notHold;
         }
     }
