5ec20a94be976d2d776f679d19e90748aefd859d
JiaRuiShao <jiaruishaojessie@gmail.com>
2026-01-07 21:20:03 -0800

    409. Longest Palindrome

--- _409.java
+++ _409.java
@@ -2,25 +2,12 @@ import java.util.HashSet;
 import java.util.Set;
 
 /**
- * 409. Longest Palindrome.
- * Given a string s which consists of lowercase or uppercase letters, return
- * the length of the longest palindrome that can be built with those letters.
- * Letters are case sensitive, for example, "Aa" is not considered a palindrome here.
- * *
- * Example:
- * Input: s = "abccccdd"
- * Output: 7
- * Explanation: One longest palindrome that can be built is "dccaccd", whose length is 7.
+ * 409. Longest Palindrome
  */
 public class _409 {
     class Solution1_HashSet {
-        /**
-         * Time: O(n)
-         * Space: o(n/2)
-         *
-         * @param s input str
-         * @return len of longest palindrome
-         */
+        // Time: O(n)
+        // Space: O(1)
         public int longestPalindrome(String s) {
             if (s == null || s.length() == 0) return 0;
             Set<Character> hs = new HashSet<Character>();
@@ -38,6 +25,8 @@ public class _409 {
         }
     }
 
+    // Time: O(n)
+    // Space: O(1)
     class Solution2_Array {
         public int longestPalindrome(String s) {
             // freq array to store the chars & their freq
@@ -65,27 +54,20 @@ public class _409 {
         }
 
         public int longestPalindrome2(String s) {
-            int[] counts = new int[56];
+            int[] freq = new int[128];
             for (char c : s.toCharArray()) {
-                if (Character.isUpperCase(c)) {
-                    counts[c - 'A' + 33]++;
-                } else {
-                    counts[c - 'a']++;
+                freq[c]++;
+            }
+
+            boolean hasOddFreq = false;
+            int pair = 0;
+            for (int i = 0; i < freq.length; i++) {
+                pair += freq[i] / 2;
+                if (freq[i] % 2 == 1) {
+                    hasOddFreq = true;
                 }
             }
-            boolean hasOdd = false;
-            int len = 0;
-            for (int i = 0; i < 56; i++) {
-                if (counts[i] % 2 != 0) {
-                    hasOdd = true;
-                    if (counts[i] > 1) {
-                        len += counts[i] - 1;
-                    }
-                } else {
-                    len += counts[i];
-                }
-            }
-            return hasOdd ? len + 1 : len;
+            return hasOddFreq ? pair * 2 + 1 : pair * 2;
         }
     }
 }
