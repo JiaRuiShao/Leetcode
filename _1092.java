347e63ea65e01fe01f274094fcc726cab819532c
JiaRuiShao <jiaruishaojessie@gmail.com>
2026-01-10 16:00:14 -0800

    1092. Shortest Common Supersequence

--- _1092.java
+++ _1092.java
@@ -3,10 +3,9 @@
  */
 public class _1092 {
     public String shortestCommonSupersequence(String str1, String str2) {
-        int m = str1.length();
-        int n = str2.length();
+        int m = str1.length(), n = str2.length();
         
-        // First, compute LCS
+        // compute LCS
         int[][] dp = new int[m + 1][n + 1];
         
         for (int i = 1; i <= m; i++) {
@@ -20,35 +19,36 @@ public class _1092 {
         }
         
         // Construct SCS by backtracking
-        StringBuilder scs = new StringBuilder();
-        int i = m, j = n;
+        int len = m + n - dp[m][n];
+        char[] scs = new char[len];
+        int i = m, j = n, pos = len - 1;
         
         while (i > 0 && j > 0) {
             if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
-                // Part of LCS, include once
-                scs.append(str1.charAt(i - 1));
+                // char in LCS
+                scs[pos--] = str1.charAt(i - 1);
                 i--; j--;
             } else if (dp[i - 1][j] > dp[i][j - 1]) {
-                // Came from top, include str1[i-1]
-                scs.append(str1.charAt(i - 1));
+                // include str1[i-1]
+                scs[pos--] = str1.charAt(i - 1);
                 i--;
             } else {
-                // Came from left, include str2[j-1]
-                scs.append(str2.charAt(j - 1));
+                // include str2[j-1]
+                scs[pos--] = str2.charAt(j - 1);
                 j--;
             }
         }
         
-        // Add remaining characters
+        // add remaining characters
         while (i > 0) {
-            scs.append(str1.charAt(i - 1));
+            scs[pos--] = str1.charAt(i - 1);
             i--;
         }
         while (j > 0) {
-            scs.append(str2.charAt(j - 1));
+            scs[pos--] = str2.charAt(j - 1);
             j--;
         }
         
-        return scs.reverse().toString();
+        return new String(scs);
     }
 }
