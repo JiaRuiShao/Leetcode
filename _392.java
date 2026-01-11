545e2d0a25e85f1e9dda3b4cabe644b39f3fead2
JiaRuiShao <jiaruishaojessie@gmail.com>
2026-01-10 22:32:36 -0800

    392. Is Subsequence

--- _392.java
+++ _392.java
@@ -5,93 +5,115 @@ import java.util.Map;
 
 /**
  * 392. Is Subsequence
+ * 
+ * Clarification:
+ * - What to return if s is empty?
+ * 
+ * Followup:
+ * - Large t and need to check if many strings are subseq of t? Pre-build char freq map for t; then use BS to find corresponding index
+ * - Count num of distinct subseq of t equals s? LC 115, DP
+ * - Given two strings `s1` and `s2`, find the shortest string that has both `s1` and `s2` as subsequences? LC 1092 LCS DP + reconstruct
+ * - Find longest common subsequence between s and t? LC 128
  */
 public class _392 {
-    public boolean isSubsequence(String s, String t) {
-        if (t.length() < s.length()) {
-            return false;
+    // Time: O(n)
+    // Space: O(1)
+    class Solution1_TwoPointers {
+        public boolean isSubsequence(String s, String t) {
+            if (t.length() < s.length()) {
+                return false;
+            }
+    
+            int ps = 0, pt = 0;
+            while (ps < s.length() && pt < t.length()) {
+                if (s.charAt(ps) == t.charAt(pt)) ps++;
+                pt++;
+            }
+            return ps == s.length();
         }
-
-        int ps = 0, pt = 0;
-        while (ps < s.length() && pt < t.length()) {
-            if (s.charAt(ps) == t.charAt(pt)) ps++;
-            pt++;
-        }
-        return ps == s.length();
     }
 
-    public boolean[] isSubsequenceFollowUp(String[] strs, String t) {
-        int n = strs.length;
-        boolean[] isSubsequence = new boolean[n];
-        int[] pos = new int[n];
-        for (int i = 0; i < t.length(); i++) {
-            char c = t.charAt(i);
+    // Time: O(n)
+    // Space: O(n)
+    class Solution2_Recursion {
+        public boolean isSubsequence(String s, String t) {
+            return helper(s, t, 0, 0);
+        }
+        
+        private boolean helper(String s, String t, int i, int j) {
+            // Base cases
+            if (i == s.length()) {
+                return true;  // Matched all of s
+            }
+            if (j == t.length()) {
+                return false;  // Ran out of t without matching all of s
+            }
+            
+            // If characters match, advance both pointers
+            if (s.charAt(i) == t.charAt(j)) {
+                return helper(s, t, i + 1, j + 1);
+            }
+            
+            // Otherwise, only advance t pointer
+            return helper(s, t, i, j + 1);
+        }
+    }
+
+    // Time: O(n+mlogn)
+    // Space: O(n)
+    class Followup_MultipleS {
+        public boolean isSubsequence(String s, String t) {
+            Map<Character, List<Integer>> indices = new HashMap<>();
+            for (int i = 0; i < t.length(); i++) {
+                indices.computeIfAbsent(t.charAt(i), k -> new ArrayList<>()).add(i);
+            }
+
+            int prevIdx = -1; // leftmost index in t for last char in s
+            for (int j = 0; j < s.length(); j++) {
+                char c = s.charAt(j);
+                if (!indices.containsKey(c)) {
+                    return false;
+                }
+                // find next occurrence after prevIdx
+                int currIdx = findSmallestIndexGTE(indices.get(c), prevIdx + 1);
+                if (currIdx == -1) {
+                    return false;
+                }
+                prevIdx = currIdx;
+            }
+            return true;
+        }
+
+        private int findSmallestIndexGTE(List<Integer> indices, int k) {
+            int lo = 0, hi = indices.size() - 1;
+            while (lo <= hi) {
+                int mid = lo + (hi - lo) / 2;
+                int index = indices.get(mid);
+                if (index == k) {
+                    return index;
+                } else if (index < k) {
+                    lo = mid + 1;
+                } else {
+                    hi = mid - 1;
+                }
+            }
+            return lo == indices.size() ? -1 : indices.get(lo);
+        }
+    }
+
+    class Followup_CountSebseq {
+        // Recurrence:
+        // dp[i][j] += dp[i-1][j-1] if s[i-1] matches t[j-1]
+        //           = dp[i][j-1] not use this char
+        public int countSubseq(String s, String t) {
+            int m = s.length(), n = t.length();
+            // dp[i][j]: ways to form s[0..i-1] using t[0..j-1]
+            int[][] dp = new int[m + 1][n + 1];
             for (int j = 0; j < n; j++) {
-                String str = strs[j];
-                if (pos[j] < str.length() && str.charAt(pos[j]) == c) {
-                    pos[j]++;
-                }
+                dp[0][j] = 1;
             }
+            
+            for (int j = )
         }
-        for (int j = 0; j < n; j++) {
-            if (pos[j] == strs[j].length()) isSubsequence[j] = true;
-        }
-        return isSubsequence;
     }
-
-    public boolean[] isSubsequenceFollowUp_Binary_Search(String[] strs, String t) {
-        Map<Character, List<Integer>> indexMap = new HashMap<>();
-
-        // Step 1: Preprocess `t`
-        for (int i = 0; i < t.length(); i++) {
-            char c = t.charAt(i);
-            indexMap.computeIfAbsent(c, k -> new ArrayList<>()).add(i);
-        }
-
-        boolean[] result = new boolean[strs.length];
-
-        // Step 2: Check each string in `strs`
-        for (int i = 0; i < strs.length; i++) {
-            String s = strs[i];
-            int prevIdx = -1; // Last matched index in `t`
-            boolean isSubseq = true;
-
-            for (char c : s.toCharArray()) {
-                if (!indexMap.containsKey(c)) {
-                    isSubseq = false;
-                    break;
-                }
-
-                List<Integer> indices = indexMap.get(c);
-                int nextIdx = findNextIndex(indices, prevIdx);
-
-                if (nextIdx == -1) {
-                    isSubseq = false;
-                    break;
-                }
-
-                prevIdx = nextIdx;
-            }
-
-            result[i] = isSubseq;
-        }
-
-        return result;
-    }
-
-    // Binary search: find smallest index in list > prevIdx
-    private int findNextIndex(List<Integer> indices, int prevIdx) {
-        int left = 0, right = indices.size() - 1, ans = -1;
-        while (left <= right) {
-            int mid = (left + right) / 2;
-            if (indices.get(mid) > prevIdx) {
-                ans = indices.get(mid);
-                right = mid - 1;
-            } else {
-                left = mid + 1;
-            }
-        }
-        return ans;
-    }
-
 }
