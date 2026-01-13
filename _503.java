5109362dda3aa9ac03093d43d155d0da022b8f29
JiaRuiShao <jiaruishaojessie@gmail.com>
2026-01-13 10:47:29 -0800

    503. Next Greater Element II

--- _503.java
+++ _503.java
@@ -5,18 +5,19 @@ import java.util.Deque;
  * 503. Next Greater Element II
  */
 public class _503 {
+    // Mono decreasing stack to find next greater elements
     class Solution1_Mono_Stack {
         public int[] nextGreaterElements(int[] nums) {
             int n = nums.length;
             int[] ngl = new int[n];
-            Deque<Integer> stk = new ArrayDeque<>(); // mono increasing stack
+            Deque<Integer> minStack = new ArrayDeque<>();
             for (int i = 2 * n - 1; i >= 0; i--) {
                 int num = nums[i % n];
-                while (!stk.isEmpty() && stk.peek() <= num) {
-                    stk.pop();
+                while (!minStack.isEmpty() && minStack.peek() <= num) {
+                    minStack.pop();
                 }
-                ngl[i % n] = stk.isEmpty() ? -1 : stk.peek();
-                stk.push(num);
+                ngl[i % n] = minStack.isEmpty() ? -1 : minStack.peek();
+                minStack.push(num);
             }
             return ngl;
         }
