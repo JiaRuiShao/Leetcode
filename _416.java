efb9bcef812e973ee0953ea43157701c36fe04e8
JiaRuiShao <jiaruishaojessie@gmail.com>
2026-01-15 15:46:38 -0800

    416. Partition Equal Subset Sum

--- _416.java
+++ _416.java
@@ -4,10 +4,12 @@ import java.util.List;
 /**
  * 416. Partition Equal Subset Sum
  * 
- * This is a 0/1 Knapsack Problem
+ * This is a 0/1 Knapsack Problem, like coin limited variant of LC 518
  * - each num used at most once
  * - capacity is target sum
- * - exists achieve target?
+ * - exists achieve target
+ * 
+ * Starts with 2D approach, then optimize into 1D array.
  * 
  * Followup:
  * - Return the actual partition subsets? Use backtrack or 2D DP
@@ -34,29 +36,99 @@ public class _416 {
         }
     }
 
-    // Time: O(nk)
+    // dp[i][j] = if we can make subset sum j using first i elem in nums
+    // dp[i][j] = dp[i-1][j] || dp[i-1][j-nums[i-1]]
+    // Time: O(nk) where k = sum / 2
     // Space: O(nk)
     class Solution2_DP_2D {
+        // can we form a subset with sum = target?
         public boolean canPartition(int[] nums) {
-            int n = nums.length, total = Arrays.stream(nums).sum();
-            if (total % 2 != 0) return false;
-            int target = total / 2;
-            boolean[][] dp = new boolean[n + 1][target + 1]; // subset exist for sum j using first i num
-            // dp[...][0] = true
-            // others default value as false
+            int sum = 0;
+            for (int num : nums) {
+                sum += num;
+            }
+            if (sum % 2 != 0) {
+                return false;
+            }
+            sum /= 2;
+            int n = nums.length;
+            // dp[i][j] = if we can make subset sum j using first i elem in nums
+            boolean[][] dp = new boolean[n + 1][sum + 1];
+            // dp[i][0] = true
+            // dp[0][1..j] = false
             for (int i = 0; i <= n; i++) {
-                Arrays.fill(dp[i], false);
                 dp[i][0] = true;
             }
-
+            // dp[i][j] = dp[i-1][j] || dp[i-1][j-nums[i-1]]
             for (int i = 1; i <= n; i++) {
                 int num = nums[i - 1];
-                for (int j = 1; j <= target; j++) {
+                for (int j = 1; j <= sum; j++) {
                     dp[i][j] = dp[i - 1][j];
-                    if (j >= num) dp[i][j] = dp[i][j] || dp[i - 1][j - num];
+                    if (j >= num) {
+                        dp[i][j] |= dp[i - 1][j - num];
+                    }
                 }
             }
-            return dp[n][target];
+            return dp[n][sum];
+        }
+
+        // Two ways to optimize 2D DP to 1D
+        // 1 - save prev row (not efficient)
+        public boolean canPartition_SpaceOptimized1(int[] nums) {
+            int sum = 0;
+            for (int num : nums) {
+                sum += num;
+            }
+            if (sum % 2 != 0) {
+                return false;
+            }
+            sum /= 2;
+            int n = nums.length;
+            // dp[i][j] = if we can make subset sum j using first i elem in nums
+            boolean[] prev = new boolean[sum + 1];
+            // dp[i][0] = true
+            // dp[0][1..j] = false
+            for (int i = 0; i <= n; i++) {
+                prev[0] = true;
+            }
+            // dp[i][j] = dp[i-1][j] || dp[i-1][j-nums[i-1]]
+            for (int i = 1; i <= n; i++) {
+                boolean[] curr = new boolean[sum + 1];
+                curr[0] = true;
+                int num = nums[i - 1];
+                for (int j = 1; j <= sum; j++) {
+                    curr[j] = prev[j];
+                    if (j >= num) {
+                        curr[j] = curr[j] || prev[j - num];
+                    }
+                }
+                prev = curr;
+            }
+            return prev[sum];
+        }
+
+        // 2 - iterate from right to left
+        public boolean canPartition_SpaceOptimized2(int[] nums) {
+            int sum = 0;
+            for (int num : nums) {
+                sum += num;
+            }
+            if (sum % 2 != 0) {
+                return false;
+            }
+            int k = sum / 2;
+            int n = nums.length;
+            boolean[] dp = new boolean[k + 1];
+            dp[0] = true;
+            // dp[i][j] depends on dp[i-1][j] and dp[i-1][j-nums[i-1]]
+            // we can iterate from right to left for inner loop iteration
+            for (int i = 1; i <= n; i++) {
+                int num = nums[i - 1];
+                for (int j = k; j >= num; j--) {
+                    dp[j] = dp[j] || dp[j - num];
+                }
+            }
+            return dp[k];
         }
     }
 
