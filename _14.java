bd8d2df32557181868c188a02420f0efba34a7fd
JiaRuiShao <jiaruishaojessie@gmail.com>
2026-01-20 22:13:16 -0800

    14. Longest Common Prefix

--- _14.java
+++ _14.java
@@ -8,30 +8,26 @@
 public class _14 {
     class Solution1 {
         public String longestCommonPrefix(String[] strs) {
-            int n = strs.length, m = strs[0].length();
-            for (int i = 0; i < m; i++) {
+            for (int i = 0; i < strs[0].length(); i++) {
                 char c = strs[0].charAt(i);
-                for (int j = 1; j < n; j++) {
-                    String curr = strs[j];
-                    if (i >= curr.length() || curr.charAt(i) != c) {
+                for (int j = 1; j < strs.length; j++) {
+                    if (i == strs[j].length() || strs[j].charAt(i) != c) {
                         return strs[0].substring(0, i);
                     }
                 }
             }
-            return strs[0]; // important!
+            return strs[0]; // **IMPORTANT **
         }
     }
 
     class Solution2 {
-        public String longestCommonPrefix(String[] strs) {
-            if (strs == null || strs.length == 0) return "";
+            public String longestCommonPrefix(String[] strs) {
             StringBuilder prefix = new StringBuilder();
-            outer: for (int i = 0; i < strs[0].length(); i++) {
+            for (int i = 0; i < strs[0].length(); i++) {
                 char c = strs[0].charAt(i);
                 for (int j = 1; j < strs.length; j++) {
-                    String str = strs[j];
-                    if (i >= str.length() || str.charAt(i) != c) {
-                        break outer;
+                    if (i == strs[j].length() || strs[j].charAt(i) != c) {
+                        return prefix.toString();
                     }
                 }
                 prefix.append(c);
