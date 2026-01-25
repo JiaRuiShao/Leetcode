f69d5370c6f14c6f171f905b9a1e45a46ceab715
JiaRuiShao <jiaruishaojessie@gmail.com>
2026-01-25 01:40:53 -0800

    242. Valid Anagram

--- _242.java
+++ _242.java
@@ -56,6 +56,20 @@ public class _242 {
             }
             return true;
         }
+
+        public boolean isAnagram2(String s, String t) {
+            if (s.length() != t.length()) return false;
+            int[] freq = new int[26];
+            for (int i = 0; i < s.length(); i++) {
+                freq[s.charAt(i) - 'a']++;
+            }
+
+            for (int i = 0; i < t.length(); i++) {
+                if (--freq[t.charAt(i) - 'a'] < 0) return false;
+            }
+
+            return true;
+        }
     }
 
     static class Solution3_Sorting {
