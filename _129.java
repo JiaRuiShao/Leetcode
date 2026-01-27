abf40395740ec166064cb61070ccf5a31d11c409
JiaRuiShao <jiaruishaojessie@gmail.com>
2026-01-26 23:08:04 -0800

    129. Sum Root to Leaf Numbers

--- _129.java
+++ _129.java
@@ -1,26 +1,9 @@
+import helper.TreeNode;
+
+/**
+ * 129. Sum Root to Leaf Numbers
+ */
 public class _129 {
-    private static class TreeNode {
-        int val;
-        TreeNode left;
-        TreeNode right;
-
-        TreeNode() {
-        }
-
-        TreeNode(int val) {
-            this.val = val;
-        }
-
-        TreeNode(int val, TreeNode left, TreeNode right) {
-            this.val = val;
-            this.left = left;
-            this.right = right;
-        }
-    }
-
-    /**
-     * Backtracking DFS Traversal. (Traversal Framework)
-     */
     class Solution1 {
         int sum = 0;
         StringBuilder path = new StringBuilder();
